d5ab9b9fe5f81f342b56cbaea4676d8798e7e587
David Rugendyke <david@mail.rocketpool.net>
2017-08-30 14:08:46 +1000

    updated nodes to 0.4.11 solidity and optimised

--- contracts/RocketNode.sol
+++ contracts/RocketNode.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.11;
 
 import "./RocketHub.sol";
 import "./interface/RocketSettingsInterface.sol";
@@ -13,12 +13,14 @@ contract RocketNode is Owned {
 
     /**** Properties ***********/
 
-    // Hub address
-    address private rocketHubAddress;
-    // The current version of this contract
-    uint8 private version;
-    // Miniumum balance a node must have to cover gas costs for smart node services when registered
-    uint private minNodeWei;
+    address private rocketHubAddress;                   // Hub address
+    uint8 private version;                              // The current version of this contract                                   
+    uint private minNodeWei;                            // Miniumum balance a node must have to cover gas costs for smart node services when registered
+
+
+    /*** Contracts **************/
+
+    RocketHub rocketHub = RocketHub(0);                 // The main RocketHub contract where primary persistant storage is maintained
 
               
     /*** Events ****************/
@@ -39,8 +41,7 @@ contract RocketNode is Owned {
 
     /// @dev Only allow access from the latest version of the RocketPool contract
     modifier onlyLatestRocketPool() {
-        RocketHub rocketHub = RocketHub(rocketHubAddress);
-        if (msg.sender != rocketHub.getRocketPoolAddress()) throw;
+        assert (msg.sender == rocketHub.getRocketPoolAddress());
         _;
     }
 
@@ -50,7 +51,9 @@ contract RocketNode is Owned {
     /// @dev pool constructor
     function RocketNode(address deployedRocketHubAddress) {
         // Set the address of the main hub
-        rocketHubAddress = deployedRocketHubAddress;
+        rocketHubAddress = deployedRocketHubAddress;    
+        // Update the contract address
+        rocketHub = RocketHub(deployedRocketHubAddress);
         // Set the min eth needed for a node account to cover gas costs
         minNodeWei = 5 ether;
     }
@@ -58,7 +61,7 @@ contract RocketNode is Owned {
 
     /// @dev Set the min eth required for a node to be registered
     /// @param amountInWei The amount in Wei
-    function setMinNodeWei(uint amountInWei) public onlyOwner  {
+    function setMinNodeWei(uint amountInWei) public onlyOwner {
         minNodeWei = amountInWei;
     }
 
@@ -66,8 +69,6 @@ contract RocketNode is Owned {
     /// @dev Get an available node for a pool to be assigned too
     // TODO: As well as assigning pools by node user server load, assign by node geographic region to aid in redundancy and decentralisation 
     function nodeAvailableForPool() public onlyLatestRocketPool returns(address) {
-        // This is called only by registered Rocket mini pools 
-        RocketHub rocketHub = RocketHub(rocketHubAddress);
         // Get all the current registered nodes
         uint256 nodeCount = rocketHub.getRocketNodeCount();
         // Create an array at the length of the current nodes, then populate it
@@ -76,43 +77,35 @@ contract RocketNode is Owned {
         address nodeAddressToUse = 0;
         uint256 prevAverageLoad = 0;
         // Retreive each node address now by index since we can't return a variable sized array from an external contract yet
-        if(nodes.length > 0) {
-            for(uint32 i = 0; i < nodes.length; i++) {
-                // Get our node address
-                address currentNodeAddress = rocketHub.getRocketNodeByIndex(i);
-                // Get the node details
-                uint256 averageLoad =  rocketHub.getRocketNodeAverageLoad(currentNodeAddress);
-                bool active =  rocketHub.getRocketNodeActive(currentNodeAddress);
-                // Get the node with the lowest current work load average to help load balancing and avoid assigning to any servers currently not activated
-                // A node must also have checked in at least once before being assinged, hence why the averageLoad must be greater than zero
-                nodeAddressToUse = (averageLoad <= prevAverageLoad || i == 0) && averageLoad > 0  && active == true ? currentNodeAddress : nodeAddressToUse;
-                prevAverageLoad = averageLoad;
-            }
-            // We have an address to use, excellent, assign it
-            if(nodeAddressToUse != 0) {
-                return nodeAddressToUse;
-            }
+        assert(nodes.length > 0);
+        // Now loop through each
+        for (uint32 i = 0; i < nodes.length; i++) {
+            // Get our node address
+            address currentNodeAddress = rocketHub.getRocketNodeByIndex(i);
+            // Get the node details
+            uint256 averageLoad = rocketHub.getRocketNodeAverageLoad(currentNodeAddress);
+            bool active = rocketHub.getRocketNodeActive(currentNodeAddress);
+            // Get the node with the lowest current work load average to help load balancing and avoid assigning to any servers currently not activated
+            // A node must also have checked in at least once before being assinged, hence why the averageLoad must be greater than zero
+            nodeAddressToUse = (averageLoad <= prevAverageLoad || i == 0) && averageLoad > 0 && active == true ? currentNodeAddress : nodeAddressToUse;
+            prevAverageLoad = averageLoad;
         }
-        // No registered nodes yet
-        throw; 
+        // We have an address to use, excellent, assign it
+        if (nodeAddressToUse != 0) {
+            return nodeAddressToUse;
+        }
     } 
 
 
     /// @dev Register a new node address if it doesn't exist, only the contract creator can do this
     /// @param nodeAccountAddressToRegister New nodes coinbase address
-    function nodeRegister(address nodeAccountAddressToRegister, string oracleID, string instanceID) public onlyOwner  {
+    function nodeRegister(address nodeAccountAddressToRegister, string oracleID, string instanceID) public onlyOwner {
         // Get the balance of the node, must meet the min requirements to service gas costs for checkins, oracle services etc
-        if(nodeAccountAddressToRegister.balance >= minNodeWei) {
-            // Add the node to the primary persistent storage so any contract upgrades won't effect the current stored nodes
-            RocketHub rocketHub = RocketHub(rocketHubAddress);
-            // Sets the rocket node if the address is ok and isn't already set
-            if(rocketHub.setRocketNode(nodeAccountAddressToRegister, sha3(oracleID), sha3(instanceID))) {
-                // Fire the event
-                NodeRegistered(nodeAccountAddressToRegister, now);
-            }
-        }else{
-            throw;
-        }
+        assert(nodeAccountAddressToRegister.balance >= minNodeWei);
+        // Sets the rocket node if the address is ok and isn't already set
+        assert(rocketHub.setRocketNode(nodeAccountAddressToRegister, sha3(oracleID), sha3(instanceID)) == true);
+        // Fire the event
+        NodeRegistered(nodeAccountAddressToRegister, now);
 	}
 
 
@@ -121,7 +114,6 @@ contract RocketNode is Owned {
     /// @param activeStatus The status to set the node
     function nodeSetActiveStatus(address nodeAddress, bool activeStatus) public onlyOwner {
         // Get our RocketHub contract with the node storage, so we can check the node is legit
-        RocketHub rocketHub = RocketHub(rocketHubAddress);
         rocketHub.setRocketNodeActive(nodeAddress, activeStatus);
     }
 
@@ -129,19 +121,13 @@ contract RocketNode is Owned {
     /// @dev Remove a node from the Rocket Pool network
     function nodeRemove(address nodeAddress) public onlyOwner {
         // Get the hub
-        RocketHub rocketHub = RocketHub(rocketHubAddress);
         RocketPoolInterface rocketPool = RocketPoolInterface(rocketHub.getRocketPoolAddress());
         // Check the node doesn't currently have any registered mini pools associated with it
-        if(rocketPool.getPoolsFilterWithNodeCount(nodeAddress) == 0) {
-            // Sets the rocket partner if the address is ok and isn't already set
-            if(rocketHub.setRocketNodeRemove(nodeAddress)) {
-                // Fire the event
-                NodeRemoved(nodeAddress, now);
-            }
-        }else{
-            throw;
-        }
+        assert(rocketPool.getPoolsFilterWithNodeCount(nodeAddress) == 0);
+        // Sets the rocket partner if the address is ok and isn't already set
+        assert(rocketHub.setRocketNodeRemove(nodeAddress) == true);
+        // Fire the event
+        NodeRemoved(nodeAddress, now);
     } 
-
 
 }
