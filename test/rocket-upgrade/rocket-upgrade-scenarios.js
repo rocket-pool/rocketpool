264fb9574538390fbd008b6de21774eead901677
Jake Pospischil <moles1@gmail.com>
2018-03-21 13:45:17 +1000

    minor cleanup

--- test/rocket-upgrade/rocket-upgrade-scenarios.js
+++ test/rocket-upgrade/rocket-upgrade-scenarios.js
@@ -3,7 +3,7 @@ import { RocketUpgrade } from "../artifacts";
 // Runs upgrade contract scenario
 export async function scenarioUpgradeContract({contractName, upgradedContractAddress, forceEther = false, forceTokens = false, fromAddress}) {
 
-	// Initialise upgrade contract
+	// Get deployed upgrade contract
 	const rocketUpgrade = await RocketUpgrade.deployed();
 
 	// Upgrade a contract
