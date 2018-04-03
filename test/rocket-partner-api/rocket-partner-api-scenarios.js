54dc8de83e16ab2b645817b3d608f15025b8b3d2
Jake Pospischil <moles1@gmail.com>
2018-04-03 14:47:30 +1000

    refactoring rocket partner api tests

--- test/rocket-partner-api/rocket-partner-api-scenarios.js
+++ test/rocket-partner-api/rocket-partner-api-scenarios.js
@@ -3,21 +3,21 @@ import { RocketPartnerAPI } from '../artifacts';
 
 // Registers partner and asserts that number of registered partners increased
 export async function scenarioRegisterPartner({partnerAddress, partnerName, fromAddress, gas}) {
-	const rocketPartnerAPI = await RocketPartnerAPI.deployed();
+    const rocketPartnerAPI = await RocketPartnerAPI.deployed();
 
-	// Get initial partner count
-	let partnerCountOld = await rocketPartnerAPI.getPartnerCount.call();
+    // Get initial partner count
+    let partnerCountOld = await rocketPartnerAPI.getPartnerCount.call();
 
-	// Register partner
-	await rocketPartnerAPI.partnerAdd(partnerAddress, partnerName, {
-		from: fromAddress,
-		gas: gas,
-	});
+    // Register partner
+    await rocketPartnerAPI.partnerAdd(partnerAddress, partnerName, {
+        from: fromAddress,
+        gas: gas,
+    });
 
-	// Get updated partner count
-	let partnerCountNew = await rocketPartnerAPI.getPartnerCount.call();
+    // Get updated partner count
+    let partnerCountNew = await rocketPartnerAPI.getPartnerCount.call();
 
-	// Assert that updated partner count is correct
+    // Assert that updated partner count is correct
     assert.equal(partnerCountNew.valueOf(), parseInt(partnerCountOld.valueOf()) + 1, 'Invalid number of partners registered');
 
 }
@@ -25,16 +25,16 @@ export async function scenarioRegisterPartner({partnerAddress, partnerName, from
 
 // Makes a deposit with a partner
 export async function scenarioPartnerDeposit({userAddress, stakingTimeID, fromAddress, value, gas}) {
-	const rocketPartnerAPI = await RocketPartnerAPI.deployed();
+    const rocketPartnerAPI = await RocketPartnerAPI.deployed();
 
-	// Make deposit
-	await rocketPartnerAPI.APIpartnerDeposit(userAddress, stakingTimeID, {
-		from: fromAddress,
-		value: value,
-		gas: gas,
-	});
+    // Make deposit
+    await rocketPartnerAPI.APIpartnerDeposit(userAddress, stakingTimeID, {
+        from: fromAddress,
+        value: value,
+        gas: gas,
+    });
 
-	// TODO: add assertions
+    // TODO: add assertions
 
 }
 
