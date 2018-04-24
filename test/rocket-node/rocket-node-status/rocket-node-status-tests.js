4da2e2e25c092c59b9f6f10f330be6ebbd2478f9
Darren Langley <darren@downrightdigital.io>
2018-04-24 11:14:50 +1000

    Removed contract.only

--- test/rocket-node/rocket-node-status/rocket-node-status-tests.js
+++ test/rocket-node/rocket-node-status/rocket-node-status-tests.js
@@ -256,7 +256,7 @@ export default function({owner}) {
         });
     });
 
-    contract.only('RocketNodeStatus - Withdrawal', async (accounts) => {
+    contract('RocketNodeStatus - Withdrawal', async (accounts) => {
 
         /**
          * Config
