a815efd4cc8ee690c11d6e9630db04ade529fdfb
Jake Pospischil <moles1@gmail.com>
2018-08-22 12:41:57 +1000

    implementing list storage tests

--- test/rocket-storage/rocket-storage-tests.js
+++ test/rocket-storage/rocket-storage-tests.js
@@ -31,154 +31,173 @@ export default function({owner}) {
         });
 
 
-        // Push an item onto a list
-        it(printTitle('-----', 'push an item onto a list'), async () => {
-
-            // Push items
-            await scenarioPushListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                value: '0x0000000000000000000000000000000000000001',
-                fromAddress: owner,
-                gas: 500000,
-            });
-            await scenarioPushListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                value: '0x0000000000000000000000000000000000000002',
-                fromAddress: owner,
-                gas: 500000,
-            });
-            await scenarioPushListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                value: '0x0000000000000000000000000000000000000003',
-                fromAddress: owner,
-                gas: 500000,
-            });
-
-        });
+        // Run list tests by type
+        function listTests(type, key, testValues) {
 
 
-        // Set a list item at index
-        it(printTitle('-----', 'set a list item at index'), async () => {
+            // Push an item onto a list
+            it(printTitle(type, 'push an item onto a list'), async () => {
 
-            // Set item
-            await scenarioSetListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                index: 1,
-                value: '0x0000000000000000000000000000000000000004',
-                fromAddress: owner,
-                gas: 500000,
+                // Push items
+                await scenarioPushListItem({
+                    type: type,
+                    key: key,
+                    value: testValues[0],
+                    fromAddress: owner,
+                    gas: 500000,
+                });
+                await scenarioPushListItem({
+                    type: type,
+                    key: key,
+                    value: testValues[1],
+                    fromAddress: owner,
+                    gas: 500000,
+                });
+                await scenarioPushListItem({
+                    type: type,
+                    key: key,
+                    value: testValues[2],
+                    fromAddress: owner,
+                    gas: 500000,
+                });
+
             });
 
-            // Set item at out of bounds index
-            await assertThrows(scenarioSetListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                index: 99,
-                value: '0x0000000000000000000000000000000000000099',
-                fromAddress: owner,
-                gas: 500000,
-            }), 'Set a list item with an out of bounds index');
 
-        });
+            // Set a list item at index
+            it(printTitle(type, 'set a list item at index'), async () => {
 
+                // Set item
+                await scenarioSetListItem({
+                    type: type,
+                    key: key,
+                    index: 1,
+                    value: testValues[3],
+                    fromAddress: owner,
+                    gas: 500000,
+                });
 
-        // Insert an item into a list at index
-        it(printTitle('-----', 'insert an item into a list at index'), async () => {
+                // Set item at out of bounds index
+                await assertThrows(scenarioSetListItem({
+                    type: type,
+                    key: key,
+                    index: 99,
+                    value: testValues[6],
+                    fromAddress: owner,
+                    gas: 500000,
+                }), 'Set a list item with an out of bounds index');
 
-            // Insert item
-            await scenarioInsertListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                index: 1,
-                value: '0x0000000000000000000000000000000000000005',
-                fromAddress: owner,
-                gas: 500000,
             });
 
-            // Insert item at end of list
-            let count = await rocketListStorage[`getAddressListCount`].call(web3.sha3('test.addresses'));
-            await scenarioInsertListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                index: count,
-                value: '0x0000000000000000000000000000000000000006',
-                fromAddress: owner,
-                gas: 500000,
+
+            // Insert an item into a list at index
+            it(printTitle(type, 'insert an item into a list at index'), async () => {
+
+                // Insert item
+                await scenarioInsertListItem({
+                    type: type,
+                    key: key,
+                    index: 1,
+                    value: testValues[4],
+                    fromAddress: owner,
+                    gas: 500000,
+                });
+
+                // Insert item at end of list
+                let count = await rocketListStorage[`get${type}ListCount`].call(key);
+                await scenarioInsertListItem({
+                    type: type,
+                    key: key,
+                    index: count,
+                    value: testValues[5],
+                    fromAddress: owner,
+                    gas: 500000,
+                });
+
+                // Insert item at out of bounds index
+                await assertThrows(scenarioInsertListItem({
+                    type: type,
+                    key: key,
+                    index: 99,
+                    value: testValues[6],
+                    fromAddress: owner,
+                    gas: 500000,
+                }), 'Inserted a list item with an out of bounds index');
+
             });
 
-            // Insert item at out of bounds index
-            await assertThrows(scenarioInsertListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                index: 99,
-                value: '0x0000000000000000000000000000000000000099',
-                fromAddress: owner,
-                gas: 500000,
-            }), 'Inserted a list item with an out of bounds index');
 
-        });
+            // Remove an item from an ordered list at index
+            it(printTitle(type, 'remove an item from an ordered list at index'), async () => {
 
+                // Remove item
+                await scenarioRemoveOListItem({
+                    type: type,
+                    key: key,
+                    index: 2,
+                    fromAddress: owner,
+                    gas: 500000,
+                });
 
-        // Remove an item from an ordered list at index
-        it(printTitle('-----', 'remove an item from an ordered list at index'), async () => {
+                // Remove item at out of bounds index
+                await assertThrows(scenarioRemoveOListItem({
+                    type: type,
+                    key: key,
+                    index: 99,
+                    fromAddress: owner,
+                    gas: 500000,
+                }), 'Removed a list item with an out of bounds index');
 
-            // Remove item
-            await scenarioRemoveOListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                index: 2,
-                fromAddress: owner,
-                gas: 500000,
             });
 
-            // Remove item at out of bounds index
-            await assertThrows(scenarioRemoveOListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                index: 99,
-                fromAddress: owner,
-                gas: 500000,
-            }), 'Removed a list item with an out of bounds index');
 
-        });
+            // Remove an item from an unordered list at index
+            it(printTitle(type, 'remove an item from an unordered list at index'), async () => {
 
+                // Remove item
+                await scenarioRemoveUListItem({
+                    type: type,
+                    key: key,
+                    index: 1,
+                    fromAddress: owner,
+                    gas: 500000,
+                });
 
-        // Remove an item from an unordered list at index
-        it(printTitle('-----', 'remove an item from an unordered list at index'), async () => {
+                // Remove item at end of list
+                let count = await rocketListStorage[`get${type}ListCount`].call(key);
+                await scenarioRemoveUListItem({
+                    type: type,
+                    key: key,
+                    index: count - 1,
+                    fromAddress: owner,
+                    gas: 500000,
+                });
 
-            // Remove item
-            await scenarioRemoveUListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                index: 1,
-                fromAddress: owner,
-                gas: 500000,
+                // Remove an item at out of bounds index
+                await assertThrows(scenarioRemoveUListItem({
+                    type: type,
+                    key: key,
+                    index: 99,
+                    fromAddress: owner,
+                    gas: 500000,
+                }), 'Removed a list item with an out of bounds index');
+
             });
 
-            // Remove item at end of list
-            let count = await rocketListStorage[`getAddressListCount`].call(web3.sha3('test.addresses'));
-            await scenarioRemoveUListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                index: count - 1,
-                fromAddress: owner,
-                gas: 500000,
-            });
 
-            // Remove an item at out of bounds index
-            await assertThrows(scenarioRemoveUListItem({
-                type: 'Address',
-                key: web3.sha3('test.addresses'),
-                index: 99,
-                fromAddress: owner,
-                gas: 500000,
-            }), 'Removed a list item with an out of bounds index');
+        }
 
-        });
+
+        // Run list tests
+        listTests('Address', web3.sha3('test.addresses'), [
+            '0x0000000000000000000000000000000000000001',
+            '0x0000000000000000000000000000000000000002',
+            '0x0000000000000000000000000000000000000003',
+            '0x0000000000000000000000000000000000000004',
+            '0x0000000000000000000000000000000000000005',
+            '0x0000000000000000000000000000000000000006',
+            '0x0000000000000000000000000000000000000099',
+        ]);
 
 
     });
