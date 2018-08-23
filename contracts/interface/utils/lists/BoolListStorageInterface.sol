3a3142cbd9b13d9415bffa47c2ff94ea7eeb8235
Jake Pospischil <moles1@gmail.com>
2018-08-23 13:00:33 +1000

    list storage interfaces

---
+++ contracts/interface/utils/lists/BoolListStorageInterface.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.4.24; 
+
+contract BoolListStorageInterface {
+    function getListCount(bytes32 _key) external view returns (uint);
+    function getListItem(bytes32 _key, uint _index) external view returns (bool);
+    function getListIndexOf(bytes32 _key, bool _value) external view returns (int);
+    function setListItem(bytes32 _key, uint _index, bool _value) external;
+    function pushListItem(bytes32 _key, bool _value) external;
+    function insertListItem(bytes32 _key, uint _index, bool _value) external;
+    function removeUnorderedListItem(bytes32 _key, uint _index) external;
+    function removeOrderedListItem(bytes32 _key, uint _index) external;
+}
