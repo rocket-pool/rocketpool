072ca0035ab188c6560a2a579d89aa8b75cdbe90
Jake Pospischil <moles1@gmail.com>
2018-09-05 15:25:00 +1000

    minor fix

--- contracts/interface/utils/lists/Bytes32QueueStorageInterface.sol
+++ contracts/interface/utils/lists/Bytes32QueueStorageInterface.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.4.24; 
 
 contract Bytes32QueueStorageInterface {
-    function getQueueLength(bytes32 _key) external view returns (uint);
+    function getQueueLength(bytes32 _key) public view returns (uint);
     function getQueueItem(bytes32 _key, uint _index) external view returns (bytes32);
     function enqueueItem(bytes32 _key, bytes32 _value) external;
     function dequeueItem(bytes32 _key) external;
