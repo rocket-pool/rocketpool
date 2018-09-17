4509e88dfe40d5ad49f180d432c4042e0adb4df9
Jake Pospischil <moles1@gmail.com>
2018-09-17 17:13:59 +1000

    implementing rocket node task events

--- test/rocket-node/rocket-node-task-scenarios.js
+++ test/rocket-node/rocket-node-task-scenarios.js
@@ -43,3 +43,58 @@ export async function scenarioAddNodeTask({taskAddress, fromAddress, gas}) {
     });
 
 }
+
+
+// Remove a node task
+export async function scenarioRemoveNodeTask({taskIndex, fromAddress, gas}) {
+    const rocketNodeTasks = await RocketNodeTasks.deployed();
+
+    // Get task address at index
+    let taskAddress = await rocketNodeTasks.getTaskAddressAt.call(taskIndex);
+
+    // Get initial task address list
+    let taskAddresses1 = await getTaskAddresses();
+
+    // Remove task
+    await rocketNodeTasks.remove(taskIndex, {from: fromAddress, gas: gas});
+
+    // Get updated task address list
+    let taskAddresses2 = await getTaskAddresses();
+
+    // Asserts
+    assert.equal(taskAddresses2.length, taskAddresses1.length - 1, 'Task list count was not updated correctly');
+    assert.equal(taskAddresses2.indexOf(taskAddress), -1, 'Task was not removed correctly');
+    taskAddresses1.forEach((address) => {
+        if (address == taskAddress) return;
+        assert.notEqual(taskAddresses2.indexOf(address), -1, 'Task was removed which should not have been');
+    });
+
+}
+
+
+// Update a node task
+export async function scenarioUpdateNodeTask({taskAddress, taskIndex, fromAddress, gas}) {
+    const rocketNodeTasks = await RocketNodeTasks.deployed();
+
+    // Get task address at index
+    let oldTaskAddress = await rocketNodeTasks.getTaskAddressAt.call(taskIndex);
+
+    // Get initial task address list
+    let taskAddresses1 = await getTaskAddresses();
+
+    // Update task
+    await rocketNodeTasks.update(taskAddress, taskIndex, {from: fromAddress, gas: gas});
+
+    // Get updated task address list
+    let taskAddresses2 = await getTaskAddresses();
+
+    // Asserts
+    assert.equal(taskAddresses2.length, taskAddresses1.length, 'Task list count changed and should not have');
+    assert.equal(taskAddresses1[taskIndex], oldTaskAddress, 'Old task address not found in initial list');
+    assert.equal(taskAddresses2[taskIndex], taskAddress, 'Task was not updated correctly');
+    taskAddresses1.forEach((address, index) => {
+        if (index == taskIndex) return;
+        assert.equal(taskAddresses1[index], taskAddresses2[index], 'Task was changed which should not have been');
+    });
+
+}
--- test/rocket-node/rocket-node-task-tests.js
+++ test/rocket-node/rocket-node-task-tests.js
@@ -1,6 +1,6 @@
 import { printTitle, assertThrows } from '../_lib/utils/general';
 import { TestNodeTask } from '../_lib/artifacts';
-import { scenarioAddNodeTask } from './rocket-node-task-scenarios';
+import { scenarioAddNodeTask, scenarioRemoveNodeTask, scenarioUpdateNodeTask } from './rocket-node-task-scenarios';
 
 export default function() {
 
@@ -16,11 +16,13 @@ export default function() {
         let testNodeTask2;
         let testNodeTask3;
         let testNodeTask1v2;
+        let testNodeTask2v2;
         before(async () => {
             testNodeTask1 = await TestNodeTask.new('NodeTask1', {gas: 5000000, gasPrice: 10000000000, from: owner});
             testNodeTask2 = await TestNodeTask.new('NodeTask2', {gas: 5000000, gasPrice: 10000000000, from: owner});
             testNodeTask3 = await TestNodeTask.new('NodeTask3', {gas: 5000000, gasPrice: 10000000000, from: owner});
             testNodeTask1v2 = await TestNodeTask.new('NodeTask1v2', {gas: 5000000, gasPrice: 10000000000, from: owner});
+            testNodeTask2v2 = await TestNodeTask.new('NodeTask2v2', {gas: 5000000, gasPrice: 10000000000, from: owner});
         });
 
 
@@ -28,6 +30,37 @@ export default function() {
         it(printTitle('owner', 'can add a node task'), async () => {
             await scenarioAddNodeTask({
                 taskAddress: testNodeTask1.address,
+                fromAddress: owner,
+                gas: 500000,
+            });
+            await scenarioAddNodeTask({
+                taskAddress: testNodeTask2.address,
+                fromAddress: owner,
+                gas: 500000,
+            });
+            await scenarioAddNodeTask({
+                taskAddress: testNodeTask3.address,
+                fromAddress: owner,
+                gas: 500000,
+            });
+        });
+
+
+        // Owner can remove a node task
+        it(printTitle('owner', 'can remove a node task'), async () => {
+            await scenarioRemoveNodeTask({
+                taskIndex: 1,
+                fromAddress: owner,
+                gas: 500000,
+            });
+        });
+
+
+        // Owner can update a node task
+        it(printTitle('owner', 'can update a node task'), async () => {
+            await scenarioUpdateNodeTask({
+                taskAddress: testNodeTask1v2.address,
+                taskIndex: 0,
                 fromAddress: owner,
                 gas: 500000,
             });
