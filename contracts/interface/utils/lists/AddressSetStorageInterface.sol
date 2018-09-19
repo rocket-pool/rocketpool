ca4ceaac34b6e54f4d016c66c727c5e2d01134e3
Jake Pospischil <moles1@gmail.com>
2018-09-19 13:30:39 +1000

    implementing set storage

---
+++ contracts/interface/utils/lists/AddressSetStorageInterface.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.4.24; 
+
+contract AddressSetStorageInterface {
+    function getCount(bytes32 _key) external view returns (uint);
+	function getItem(bytes32 _key, uint _index) external view returns (address);
+	function getIndexOf(bytes32 _key, address _value) external view returns (int);
+	function addItem(bytes32 _key, address _value) external;
+	function removeItem(bytes32 _key, address _value) external;
+}
