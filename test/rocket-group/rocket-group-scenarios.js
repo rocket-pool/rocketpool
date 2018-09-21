d3e21caaf04f47d4a11bb0b734bd47f19baa8a7b
Jake Pospischil <moles1@gmail.com>
2018-09-21 11:50:03 +1000

    implementing rocket group tests

--- test/rocket-group/rocket-group-scenarios.js
+++ test/rocket-group/rocket-group-scenarios.js
@@ -1,12 +1,26 @@
 // Dependencies
-import { RocketGroupAPI } from '../_lib/artifacts';
+import { RocketGroupAPI, RocketGroupSettings } from '../_lib/artifacts';
 
 
 // Add a group
 export async function scenarioAddGroup({name, stakingFee, value, fromAddress, gas}) {
     const rocketGroupAPI = await RocketGroupAPI.deployed();
+    const rocketGroupSettings = await RocketGroupSettings.deployed();
+
+    // Get group creation fee address
+    let feeAddress = await rocketGroupSettings.getNewFeeAddress();
+
+    // Get initial group creation fee address balance
+    let feeBalance1 = parseInt(await web3.eth.getBalance(feeAddress));
 
     // Add group
     let result = await rocketGroupAPI.add(name, stakingFee, {from: fromAddress, gas: gas, value: value});
+
+    // Get updated group creation fee address balance
+    let feeBalance2 = parseInt(await web3.eth.getBalance(feeAddress));
+
+    // Asserts
+    assert.equal(result.logs.filter(log => (log.event == 'GroupAdd')).length, 1, 'Group was not created successfully');
+    assert.equal(feeBalance2, feeBalance1 + value, 'Creation fee balance was not transferred successfully');
 
 }
