32cc27d588ed43d49e7dbe5bbfa6281781e6ddfa
Jake Pospischil <moles1@gmail.com>
2018-09-26 12:53:45 +1000

    don't modify node availability indexes on trusted status change; nodes can be in both indexes at once

--- contracts/RocketAdmin.sol
+++ contracts/RocketAdmin.sol
@@ -4,7 +4,6 @@ pragma solidity 0.4.24;
 import "./RocketBase.sol";
 // Interfaces
 import "./interface/RocketStorageInterface.sol";
-import "./interface/utils/lists/AddressSetStorageInterface.sol";
 
 
 /// @title Admin only methods for Rocket Pool owner and admins
@@ -12,8 +11,6 @@ import "./interface/utils/lists/AddressSetStorageInterface.sol";
 contract RocketAdmin is RocketBase {
 
     /*** Contracts **************/
-
-    AddressSetStorageInterface addressSetStorage = AddressSetStorageInterface(0);
 
   
     /*** Events ****************/
@@ -41,17 +38,8 @@ contract RocketAdmin is RocketBase {
         // Check it exists first and isn't already the specified status
         require(rocketStorage.getBool(keccak256(abi.encodePacked("node.exists", _nodeAddress))), "Node address does not exist.");
         require(rocketStorage.getBool(keccak256(abi.encodePacked("node.trusted", _nodeAddress))) != _trusted, "Node trusted status already set.");
-        // Get contracts
-        addressSetStorage = AddressSetStorageInterface(getContractAddress("utilAddressSetStorage"));
         // Set now
         rocketStorage.setBool(keccak256(abi.encodePacked("node.trusted", _nodeAddress)), _trusted);
-        // Update available node indexes
-        bytes32 oldAvailableKey = keccak256(abi.encodePacked("nodes.available", "trusted", !_trusted));
-        bytes32 newAvailableKey = keccak256(abi.encodePacked("nodes.available", "trusted", _trusted));
-        if (addressSetStorage.getIndexOf(oldAvailableKey, _nodeAddress) != -1) {
-            addressSetStorage.removeItem(oldAvailableKey, _nodeAddress);
-            addressSetStorage.addItem(newAvailableKey, _nodeAddress);
-        }
         // Return success flag
         return true;
     }
