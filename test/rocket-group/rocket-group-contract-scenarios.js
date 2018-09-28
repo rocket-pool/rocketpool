4816315ff2385fadcb5c2c1a83f1f03021d8c043
Jake Pospischil <moles1@gmail.com>
2018-09-28 16:25:19 +1000

    implementing group contract depositor & withdrawer addition/removal tests

--- test/rocket-group/rocket-group-contract-scenarios.js
+++ test/rocket-group/rocket-group-contract-scenarios.js
@@ -11,3 +11,64 @@ export async function scenarioSetFeePerc({groupContract, stakingFee, fromAddress
     assert.equal(feePerc, stakingFee, 'Staking fee was not successfully updated');
 
 }
+
+
+// Add a depositor to the group
+export async function scenarioAddDepositor({groupContract, depositorAddress, fromAddress, gas}) {
+
+    // Add depositor
+    await groupContract.addDepositor(depositorAddress, {from: fromAddress, gas: gas});
+
+    // Check depositor
+    let hasDepositor = await groupContract.hasDepositor.call(depositorAddress);
+
+    // Asserts
+    assert.isTrue(hasDepositor, 'Depositor was not added successfully');
+
+}
+
+
+// Remove a depositor from the group
+export async function scenarioRemoveDepositor({groupContract, depositorAddress, fromAddress, gas}) {
+
+    // Remove depositor
+    await groupContract.removeDepositor(depositorAddress, {from: fromAddress, gas: gas});
+
+    // Check depositor
+    let hasDepositor = await groupContract.hasDepositor.call(depositorAddress);
+
+    // Asserts
+    assert.isFalse(hasDepositor, 'Depositor was not removed successfully');
+
+}
+
+
+// Add a withdrawer to the group
+export async function scenarioAddWithdrawer({groupContract, withdrawerAddress, fromAddress, gas}) {
+
+    // Add withdrawer
+    await groupContract.addWithdrawer(withdrawerAddress, {from: fromAddress, gas: gas});
+
+    // Check withdrawer
+    let hasWithdrawer = await groupContract.hasWithdrawer.call(withdrawerAddress);
+
+    // Asserts
+    assert.isTrue(hasWithdrawer, 'Withdrawer was not added successfully');
+
+}
+
+
+// Remove a withdrawer from the group
+export async function scenarioRemoveWithdrawer({groupContract, withdrawerAddress, fromAddress, gas}) {
+
+    // Remove withdrawer
+    await groupContract.removeWithdrawer(withdrawerAddress, {from: fromAddress, gas: gas});
+
+    // Check withdrawer
+    let hasWithdrawer = await groupContract.hasWithdrawer.call(withdrawerAddress);
+
+    // Asserts
+    assert.isFalse(hasWithdrawer, 'Withdrawer was not removed successfully');
+
+}
+
--- test/rocket-group/rocket-group-contract-tests.js
+++ test/rocket-group/rocket-group-contract-tests.js
@@ -1,6 +1,6 @@
 import { printTitle, assertThrows } from '../_lib/utils/general';
 import { RocketGroupAPI, RocketGroupContract, RocketGroupSettings } from '../_lib/artifacts';
-import { scenarioSetFeePerc } from './rocket-group-contract-scenarios';
+import { scenarioSetFeePerc, scenarioAddDepositor, scenarioRemoveDepositor, scenarioAddWithdrawer, scenarioRemoveWithdrawer } from './rocket-group-contract-scenarios';
 
 export default function() {
 
@@ -14,6 +14,9 @@ export default function() {
 
         // Setup
         let groupContract;
+        let accessor1Address;
+        let accessor2Address;
+        let accessor3Address;
         before(async () => {
 
             // Get new group fee
@@ -22,11 +25,21 @@ export default function() {
 
             // Create group
             let rocketGroupAPI = await RocketGroupAPI.deployed();
-            let result = await rocketGroupAPI.add('Group 1', web3.utils.toWei('0', 'ether'), {from: groupOwner, gas: 7500000, value: newGroupFee});
+            let groupResult = await rocketGroupAPI.add('Group 1', web3.utils.toWei('0', 'ether'), {from: groupOwner, gas: 7500000, value: newGroupFee});
 
             // Get group contract
-            let groupContractAddress = result.logs.filter(log => (log.event == 'GroupAdd'))[0].args.ID;
+            let groupContractAddress = groupResult.logs.filter(log => (log.event == 'GroupAdd'))[0].args.ID;
             groupContract = await RocketGroupContract.at(groupContractAddress);
+
+            // Create default accessor contracts
+            let accessorResult1 = await rocketGroupAPI.createDefaultAccessor(groupContractAddress, {from: groupOwner, gas: 7500000});
+            let accessorResult2 = await rocketGroupAPI.createDefaultAccessor(groupContractAddress, {from: groupOwner, gas: 7500000});
+            let accessorResult3 = await rocketGroupAPI.createDefaultAccessor(groupContractAddress, {from: groupOwner, gas: 7500000});
+
+            // Get accessor contract addresses
+            accessor1Address = accessorResult1.logs.filter(log => (log.event == 'GroupCreateDefaultAccessor'))[0].args.accessorAddress;
+            accessor2Address = accessorResult2.logs.filter(log => (log.event == 'GroupCreateDefaultAccessor'))[0].args.accessorAddress;
+            accessor3Address = accessorResult3.logs.filter(log => (log.event == 'GroupCreateDefaultAccessor'))[0].args.accessorAddress;
 
         });
 
@@ -50,6 +63,183 @@ export default function() {
                 fromAddress: groupOwner,
                 gas: 500000,
             }), 'Set an invalid fee percentage');
+        });
+
+
+        // Random account cannot set the group's fee percentage
+        it(printTitle('random account', 'cannot set the group\'s fee percentage'), async () => {
+            await assertThrows(scenarioSetFeePerc({
+                groupContract,
+                stakingFee: web3.utils.toWei('0.1', 'ether'),
+                fromAddress: accounts[9],
+                gas: 500000,
+            }), 'Random account set the group\'s fee percentage');
+        });
+
+
+        // Group owner can add a depositor to the group
+        it(printTitle('group owner', 'can add a depositor to the group'), async () => {
+            await scenarioAddDepositor({
+                groupContract,
+                depositorAddress: accessor1Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            });
+            await scenarioAddDepositor({
+                groupContract,
+                depositorAddress: accessor2Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            });
+        });
+
+
+        // Group owner cannot add an existing depositor to the group
+        it(printTitle('group owner', 'cannot add an existing depositor to the group'), async () => {
+            await assertThrows(scenarioAddDepositor({
+                groupContract,
+                depositorAddress: accessor1Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            }), 'Added an existing depositor to the group');
+        });
+
+
+        // Random account cannot add a depositor to the group
+        it(printTitle('random account', 'cannot add a depositor to the group'), async () => {
+            await assertThrows(scenarioAddDepositor({
+                groupContract,
+                depositorAddress: accessor3Address,
+                fromAddress: accounts[9],
+                gas: 500000,
+            }), 'Random account added a depositor to the group');
+        });
+
+
+        // Group owner can remove a depositor from the group
+        it(printTitle('group owner', 'can remove a depositor from the group'), async () => {
+            await scenarioRemoveDepositor({
+                groupContract,
+                depositorAddress: accessor2Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            });
+        });
+
+
+        // Group owner cannot remove a nonexistant depositor from the group
+        it(printTitle('group owner', 'cannot remove a nonexistant depositor from the group'), async () => {
+            await assertThrows(scenarioRemoveDepositor({
+                groupContract,
+                depositorAddress: accessor2Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            }), 'Removed a nonexistant depositor from the group');
+        });
+
+
+        // Random account cannot remove a depositor from the group
+        it(printTitle('random account', 'cannot remove a depositor from the group'), async () => {
+            await assertThrows(scenarioRemoveDepositor({
+                groupContract,
+                depositorAddress: accessor1Address,
+                fromAddress: accounts[9],
+                gas: 500000,
+            }), 'Random account removed a depositor from the group');
+        });
+
+
+        // Group owner can add a withdrawer to the group
+        it(printTitle('group owner', 'can add a withdrawer to the group'), async () => {
+            await scenarioAddWithdrawer({
+                groupContract,
+                withdrawerAddress: accessor1Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            });
+            await scenarioAddWithdrawer({
+                groupContract,
+                withdrawerAddress: accessor2Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            });
+        });
+
+
+        // Group owner cannot add an existing withdrawer to the group
+        it(printTitle('group owner', 'cannot add an existing withdrawer to the group'), async () => {
+            await assertThrows(scenarioAddWithdrawer({
+                groupContract,
+                withdrawerAddress: accessor1Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            }), 'Added an existing withdrawer to the group');
+        });
+
+
+        // Random account cannot add a withdrawer to the group
+        it(printTitle('random account', 'cannot add a withdrawer to the group'), async () => {
+            await assertThrows(scenarioAddWithdrawer({
+                groupContract,
+                withdrawerAddress: accessor3Address,
+                fromAddress: accounts[9],
+                gas: 500000,
+            }), 'Random account added a withdrawer to the group');
+        });
+
+
+        // Group owner can remove a withdrawer from the group
+        it(printTitle('group owner', 'can remove a withdrawer from the group'), async () => {
+            await scenarioRemoveWithdrawer({
+                groupContract,
+                withdrawerAddress: accessor2Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            });
+        });
+
+
+        // Group owner cannot remove the last withdrawer from the group
+        it(printTitle('group owner', 'cannot remove the last withdrawer from the group'), async () => {
+
+            // Attempt removal
+            await assertThrows(scenarioRemoveWithdrawer({
+                groupContract,
+                withdrawerAddress: accessor1Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            }), 'Removed the last withdrawer from the group');
+
+            // Add withdrawer
+            await scenarioAddWithdrawer({
+                groupContract,
+                withdrawerAddress: accessor3Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            });
+
+        });
+
+
+        // Group owner cannot remove a nonexistant withdrawer from the group
+        it(printTitle('group owner', 'cannot remove a nonexistant withdrawer from the group'), async () => {
+            await assertThrows(scenarioRemoveWithdrawer({
+                groupContract,
+                withdrawerAddress: accessor2Address,
+                fromAddress: groupOwner,
+                gas: 500000,
+            }), 'Removed a nonexistant withdrawer from the group');
+        });
+
+
+        // Random account cannot remove a withdrawer from the group
+        it(printTitle('random account', 'cannot remove a withdrawer from the group'), async () => {
+            await assertThrows(scenarioRemoveWithdrawer({
+                groupContract,
+                withdrawerAddress: accessor1Address,
+                fromAddress: accounts[9],
+                gas: 500000,
+            }), 'Random account removed a withdrawer from the group');
         });
 
 
