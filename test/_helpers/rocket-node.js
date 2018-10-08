349826c7be347acd1106d4ed6fcdd43de1f08d69
Jake Pospischil <moles1@gmail.com>
2018-10-08 12:29:26 +1000

    added create node minipools helper

--- test/_helpers/rocket-node.js
+++ test/_helpers/rocket-node.js
@@ -1,5 +1,6 @@
 // Dependencies
-import { RocketNodeAPI, RocketNodeContract } from '../_lib/artifacts';
+import { RocketMinipoolSettings, RocketNodeAPI, RocketNodeContract } from '../_lib/artifacts';
+import { mintRpl } from './rocket-pool-token';
 
 
 // Create a new node contract
@@ -13,6 +14,32 @@ export async function createNodeContract({timezone, nodeOperator}) {
     let nodeContractAddress = nodeAddResult.logs.filter(log => (log.event == 'NodeAdd'))[0].args.contractAddress;
     let nodeContract = await RocketNodeContract.at(nodeContractAddress);
     return nodeContract;
+
+}
+
+
+// Create minipools under a node
+export async function createNodeMinipools({nodeContract, stakingDurationID, minipoolCount, nodeOperator, owner}) {
+
+    // Get node deposit amount per minipool created
+    let rocketMinipoolSettings = await RocketMinipoolSettings.deployed();
+    let miniPoolLaunchAmount = parseInt(await rocketMinipoolSettings.getMinipoolLaunchAmount.call());
+    let nodeDepositAmount = Math.floor(miniPoolLaunchAmount / 2);
+
+    // Create minipools
+    for (let mi = 0; mi < minipoolCount; ++mi) {
+
+        // Reserve node deposit
+        await nodeContract.depositReserve(nodeDepositAmount, stakingDurationID, {from: nodeOperator, gas: 500000});
+
+        // Deposit RPL
+        let rplRequired = await nodeContract.getDepositReserveRPLRequired.call();
+        await mintRpl({toAddress: nodeContract.address, rplAmount: rplRequired, fromAddress: owner});
+
+        // Complete deposit to create minipool
+        await nodeContract.deposit({from: nodeOperator, gas: 7500000, value: nodeDepositAmount});
+
+    }
 
 }
 
