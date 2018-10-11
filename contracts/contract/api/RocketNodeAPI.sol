6a2a1a807c136e23975309c5a052688e75ba5c2a
Jake Pospischil <moles1@gmail.com>
2018-10-11 15:22:37 +1000

    increased max RPL ratio

--- contracts/contract/api/RocketNodeAPI.sol
+++ contracts/contract/api/RocketNodeAPI.sol
@@ -169,7 +169,7 @@ contract RocketNodeAPI is RocketBase {
         // Check the node operator doesn't have a reservation that's current, must wait for that to expire first or cancel it.
         require(now > (_lastDepositReservedTime + rocketNodeSettings.getDepositReservationTime()), "Only one deposit reservation can be made at a time, the current deposit reservation will expire in under 24hrs.");
         // Check the rpl ratio is valid
-        require(_rplRatio < 3 ether, "RPL Ratio is too high.");
+        require(_rplRatio < 5 ether, "RPL Ratio is too high.");
         // All ok
         return true;
     }
