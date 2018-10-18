523bf5aecb3f6f4fd84439d5bd3f675b3630424f
Jake Pospischil <moles1@gmail.com>
2018-10-18 16:27:55 +1000

    formatting fix

--- contracts/interface/casper/ValidatorRegistrationInterface.sol
+++ contracts/interface/casper/ValidatorRegistrationInterface.sol
@@ -3,8 +3,8 @@ pragma solidity 0.4.24;
 
 // Validator registration contract interface
 contract ValidatorRegistrationInterface {
-	// Properties
-	mapping (bytes32 => bool) public usedPubkey;
+    // Properties
+    mapping (bytes32 => bool) public usedPubkey;
     // Getters
     function deposit(bytes32 _pubkey, uint _withdrawalShardID, address _withdrawalAddressbytes32, bytes32 _randaoCommitment) public payable;
 }
