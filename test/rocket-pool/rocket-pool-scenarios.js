9f10a8ea4535c98fad427d3bf3014c85e095ef57
Jake Pospischil <moles1@gmail.com>
2018-11-15 13:44:23 +1000

    implemented RocketPool contract tests

---
+++ test/rocket-pool/rocket-pool-scenarios.js
@@ -0,0 +1,24 @@
+// Dependencies
+import { RocketPool } from '../_lib/artifacts';
+
+
+// Get a contract address
+export async function scenarioGetContractAddress(name) {
+    const rocketPool = await RocketPool.deployed();
+
+    // Get & return address
+    let address = await rocketPool.getContractAddress(name);
+    return address;
+
+}
+
+
+// Create minipool directly
+export async function scenarioCreateMinipool({nodeOwner, durationID, etherAmount, rplAmount, isTrusted, fromAddress, gas}) {
+    const rocketPool = await RocketPool.deployed();
+
+    // Create minipool
+    await rocketPool.minipoolCreate(nodeOwner, durationID, etherAmount, rplAmount, isTrusted, {from: fromAddress, gas: gas});
+
+}
+
--- test/rocket-pool/rocket-pool-tests.js
+++ test/rocket-pool/rocket-pool-tests.js
@@ -1,7 +1,35 @@
+import { printTitle, assertThrows } from '../_lib/utils/general';
+import { scenarioGetContractAddress, scenarioCreateMinipool } from './rocket-pool-scenarios';
 
-export default function({owner}) {
+export default function() {
 
     contract('RocketPool', async (accounts) => {
+
+
+        // Accounts
+        const owner = accounts[0];
+        const nodeOwner = accounts[1];
+
+
+        // Cannot get a nonexistant contract address
+        it(printTitle('-----', 'cannot get a nonexistant contract address'), async () => {
+            await assertThrows(scenarioGetContractAddress('nonexistant'), 'Got a nonexistant contract address');
+        });
+
+
+        // Cannot create a minipool directly
+        it(printTitle('-----', 'cannot create a minipool directly'), async () => {
+            await assertThrows(scenarioCreateMinipool({
+                nodeOwner: nodeOwner,
+                durationID: '3m',
+                etherAmount: web3.utils.toWei('1', 'ether'),
+                rplAmount: web3.utils.toWei('1', 'ether'),
+                isTrusted: false,
+                fromAddress: owner,
+            }), 'Created a minipool directly');
+        });
+
+
     });
 
 }
