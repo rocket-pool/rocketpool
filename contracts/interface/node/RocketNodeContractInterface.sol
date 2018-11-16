ba4d0b2dbbacc1f5232810bd2374631e6d5a75ef
Jake Pospischil <moles1@gmail.com>
2018-11-16 11:39:18 +1000

    RocketNodeContract fixes

--- contracts/interface/node/RocketNodeContractInterface.sol
+++ contracts/interface/node/RocketNodeContractInterface.sol
@@ -3,7 +3,8 @@ pragma solidity 0.4.24;
 
 contract RocketNodeContractInterface {
     function getOwner() public view returns(address);
-    function getLastDepositReservedTime() public view returns(uint256);
+    function getHasDepositReservation() public view returns(bool);
+    function getDepositReservedTime() public view returns(uint256);
     function getDepositReserveEtherRequired() public returns(uint256);
     function getDepositReserveRPLRequired() public returns(uint256);
     function getDepositReserveDurationID() public returns(string);
