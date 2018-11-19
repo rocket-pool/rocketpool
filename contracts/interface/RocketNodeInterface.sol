2ff7d0a9e243d1e8728121bee2d480b4e593fe89
Jake Pospischil <moles1@gmail.com>
2018-11-19 13:53:11 +1000

    formatting fix

--- contracts/interface/RocketNodeInterface.sol
+++ contracts/interface/RocketNodeInterface.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.4.24;
 
 contract RocketNodeInterface {
-	function getAvailableNodeCount(string _durationID) public returns (uint256);
+    function getAvailableNodeCount(string _durationID) public returns (uint256);
     function getRandomAvailableNode(string _durationID, uint256 _nonce) public returns (address, bool);
     function setNodeAvailable(address _nodeOwner, bool _trusted, string _durationID) public;
     function setNodeUnavailable(address _nodeOwner, bool _trusted, string _durationID) public;
