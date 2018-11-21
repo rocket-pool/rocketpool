3c05ab517a2e1d63346411dc51bfb7946b592f08
Jake Pospischil <moles1@gmail.com>
2018-11-21 10:38:45 +1000

    RocketNodeFactoryInterface.sol fix

--- contracts/interface/node/RocketNodeFactoryInterface.sol
+++ contracts/interface/node/RocketNodeFactoryInterface.sol
@@ -4,5 +4,5 @@ pragma solidity 0.5.0;
 // Our node interface
 contract RocketNodeFactoryInterface {
     // Methods
-    function createRocketNodeContract(address _nodeAddress) public view returns (address);
+    function createRocketNodeContract(address _nodeOwnerAddress) public returns(address);
 }
