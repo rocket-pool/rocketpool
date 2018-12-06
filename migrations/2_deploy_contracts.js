13aa9e92a5eb6842ebad2e841db0ccd26a817d7a
Jake Pospischil <moles1@gmail.com>
2018-12-06 11:24:54 +1000

    using address queue storage

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -47,6 +47,7 @@ contracts.rocketPoolToken = artifacts.require('./token/DummyRocketPoolToken.sol'
 // Utilities
 contracts.utilMaths = artifacts.require('./utils/Maths.sol');
 contracts.utilPublisher = artifacts.require('./Publisher.sol');
+contracts.utilAddressQueueStorage = artifacts.require('./AddressQueueStorage.sol');
 contracts.utilBytes32QueueStorage = artifacts.require('./Bytes32QueueStorage.sol');
 contracts.utilAddressSetStorage = artifacts.require('./AddressSetStorage.sol');
 contracts.utilBytes32SetStorage = artifacts.require('./Bytes32SetStorage.sol');
@@ -60,7 +61,6 @@ if (testUtils) {
   contracts.utilIntListStorage = artifacts.require('./IntListStorage.sol');
   contracts.utilStringListStorage = artifacts.require('./StringListStorage.sol');
   contracts.utilUintListStorage = artifacts.require('./UintListStorage.sol');
-  contracts.utilAddressQueueStorage = artifacts.require('./AddressQueueStorage.sol');
   contracts.utilBoolQueueStorage = artifacts.require('./BoolQueueStorage.sol');
   contracts.utilBytesQueueStorage = artifacts.require('./BytesQueueStorage.sol');
   contracts.utilIntQueueStorage = artifacts.require('./IntQueueStorage.sol');
