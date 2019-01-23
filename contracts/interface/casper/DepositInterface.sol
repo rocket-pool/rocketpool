ca6fa80495e1b7e361f48763a7969228ebfe7fb6
Jake Pospischil <moles1@gmail.com>
2019-01-23 13:51:41 +1000

    minor deposit contract interface fix

--- contracts/interface/casper/DepositInterface.sol
+++ contracts/interface/casper/DepositInterface.sol
@@ -3,5 +3,5 @@ pragma solidity 0.5.0;
 
 // Casper deposit contract interface
 contract DepositInterface {
-    function deposit(bytes _depositInput) public payable;
+    function deposit(bytes memory _depositInput) public payable;
 }
