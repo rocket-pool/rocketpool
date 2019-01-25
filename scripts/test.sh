21f89d7a359e50ed480a62d8d04ae1537b12f8aa
Jake Pospischil <moles1@gmail.com>
2019-01-25 11:10:57 +1000

    integrating beacon chain simulator

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,6 +11,10 @@ cleanup() {
   if [ -n "$ganache_pid" ] && ps -p $ganache_pid > /dev/null; then
     kill -9 $ganache_pid
   fi
+  # Kill the beacon chain simulator instance that we started (if we started one and if it's still running).
+  if [ -n "$beacon_pid" ] && ps -p $beacon_pid > /dev/null; then
+    kill -9 $beacon_pid
+  fi
 }
 
 if [ "$SOLIDITY_COVERAGE" = true ]; then
@@ -18,6 +22,12 @@ if [ "$SOLIDITY_COVERAGE" = true ]; then
 else
   ganache_port=8545
 fi
+
+# Beacon chain simulator config
+beacon_port=9545
+deposit_contract_address=0xb50eA9565646e5Ed39688694b283cb185A3CC130
+withdrawal_contract_address=0xb50eA9565646e5Ed39688694b283cb185A3CC130
+withdrawal_from_address=0xe6ed92d26573c67af5eca7fb2a49a807fb8f88db
 
 ganache_running() {
   nc -z localhost "$ganache_port"
@@ -33,11 +43,29 @@ start_ganache() {
   ganache_pid=$!
 }
 
+beacon_running() {
+  nc -z localhost "$beacon_port"
+}
+
+start_beacon() {
+  node node_modules/beacon-chain-simulator/beacon/index.js --depositContract $deposit_contract_address --withdrawalContract $withdrawal_contract_address --from $withdrawal_from_address --noDatabase > /dev/null &
+  beacon_pid=$!
+}
+
 if ganache_running; then
   echo "Using existing Ganache instance"
 else
   echo "Starting our own Ganache instance"
   start_ganache
+fi
+
+sleep 3
+
+if beacon_running; then
+  echo "Using existing beacon chain simulator instance"
+else
+  echo "Starting our own beacon chain simulator instance"
+  start_beacon
 fi
 
 if [ "$SOLIDITY_COVERAGE" = true ]; then
