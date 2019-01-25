aeb9b7318293eba60bbf9d40920ce45ed97133f6
Jake Pospischil <moles1@gmail.com>
2019-01-25 16:13:50 +1000

    beacon chain simulator integration

--- test/_lib/utils/beacon.js
+++ test/_lib/utils/beacon.js
@@ -1,8 +1,66 @@
 const ssz = require('@chainsafesystems/ssz');
+const WebSocket = require('ws');
+
+
+// Default beacon chain simulator api host
+const beaconHost = 'http://127.0.0.1:9545';
 
 
 // Current default pubkey index
 let defaultPubkeyIndex = 0;
+
+
+// Get the status of a validator on the beacon chain
+export async function getValidatorStatus(pubkey) {
+
+    // Validator status promise
+    let validatorStatus = new Promise((resolve, reject) => {
+
+        // Initialise websocket connection to beacon chain simulator api
+        let ws = new WebSocket(beaconHost);
+
+        // Request validator status on connection open
+        ws.on('open', () => {
+            ws.send(JSON.stringify({
+                message: 'get_validator_status',
+                pubkey,
+            }));
+        });
+
+        // Handle server messages
+        ws.on('message', (payload) => {
+            try {
+                let data = JSON.parse(payload);
+                switch (data.message) {
+
+                    // Validator status
+                    case 'validator_status':
+                        if (data.pubkey != pubkey) break;
+                        ws.close();
+                        resolve(data.status);
+                    break;
+
+                    // Error
+                    case 'error':
+                        ws.close();
+                        reject(new Error(data.error));
+                    break;
+
+                }
+            }
+            catch (e) {
+                ws.close();
+                reject(new Error(e.message));
+            }
+        });
+
+    });
+
+    // Return status
+    let status = await validatorStatus;
+    return status;
+
+}
 
 
 // Create a serialised DepositInput object from a given set of credentials
@@ -47,3 +105,24 @@ export function getDepositInput({pubkey, withdrawalPubkey}) {
     );
 
 }
+
+
+// Deserialise a serialised DepositInput object
+export function deserialiseDepositInput(depositInputHex) {
+
+    // Deserialise
+    let depositInputData = ssz.deserialize(Buffer.from(depositInputHex.substr(2), 'hex'), 0, {fields: [
+        ['pubkey', 'bytes48'],
+        ['withdrawal_credentials', 'bytes32'],
+        ['proof_of_possession', 'bytes96'],
+    ]});
+
+    // Return DepositInput object
+    return {
+        pubkey: depositInputData.deserializedData.pubkey.toString('hex'),
+        withdrawal_credentials: depositInputData.deserializedData.withdrawal_credentials.toString('hex'),
+        proof_of_possession: depositInputData.deserializedData.proof_of_possession.toString('hex'),
+    };
+
+}
+
