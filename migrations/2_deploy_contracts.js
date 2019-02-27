9f7751de7f1ddf58e369005ef4bd931b8d6e8417
Jake Pospischil <moles1@gmail.com>
2019-02-27 11:58:06 +1000

    deploy RPB token contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -48,6 +48,7 @@ contracts.rocketGroupSettings = artifacts.require('./settings/RocketGroupSetting
 contracts.rocketNodeSettings = artifacts.require('./settings/RocketNodeSettings.sol');
 // Dummy Contracts
 contracts.rocketPoolToken = artifacts.require('./token/DummyRocketPoolToken.sol');
+contracts.rocketBETHToken = artifacts.require('./token/RocketBETHToken.sol');
 // Node tasks
 contracts.taskDisableInactiveNodes = artifacts.require('./tasks/DisableInactiveNodes.sol');
 contracts.taskCalculateNodeFee = artifacts.require('./tasks/CalculateNodeFee.sol');
