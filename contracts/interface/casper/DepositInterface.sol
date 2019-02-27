e5a46e6b1231d183b873e084b8235c9649ae5954
Jake Pospischil <moles1@gmail.com>
2019-02-27 11:55:05 +1000

    minor cleanup

--- contracts/interface/casper/DepositInterface.sol
+++ contracts/interface/casper/DepositInterface.sol
@@ -1,7 +1,5 @@
-pragma solidity 0.5.0; 
+pragma solidity 0.5.0;
 
-
-// Casper deposit contract interface
 contract DepositInterface {
     function deposit(bytes memory _depositInput) public payable;
 }
