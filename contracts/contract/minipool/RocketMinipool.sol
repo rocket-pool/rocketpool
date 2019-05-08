9ff904da7fe60fa8b1e740b4b1ec5e59fa52dd9b
Jake Pospischil <moles1@gmail.com>
2019-05-08 17:02:36 +1000

    refactored minipool properties, structs & common methods into base class

--- contracts/contract/minipool/RocketMinipool.sol
+++ contracts/contract/minipool/RocketMinipool.sol
@@ -2,16 +2,9 @@ pragma solidity 0.5.8;
 
 
 // Interfaces
-import "../../interface/RocketPoolInterface.sol";
-import "../../interface/RocketStorageInterface.sol";
-import "../../interface/minipool/RocketMinipoolInterface.sol";
-import "../../interface/settings/RocketNodeSettingsInterface.sol";
+import "./RocketMinipoolBase.sol";
 import "../../interface/settings/RocketMinipoolSettingsInterface.sol";
-import "../../interface/casper/DepositInterface.sol";
-import "../../interface/group/RocketGroupContractInterface.sol";
 import "../../interface/node/RocketNodeContractInterface.sol";
-import "../../interface/token/ERC20.sol";
-import "../../interface/utils/pubsub/PublisherInterface.sol";
 // Libraries
 import "../../lib/SafeMath.sol";
 
@@ -19,123 +12,22 @@ import "../../lib/SafeMath.sol";
 /// @title A minipool under the main RocketPool, all major logic is contained within the RocketMinipoolDelegate contracts which are upgradable when minipools are deployed
 /// @author David Rugendyke
 
-contract RocketMinipool {
+contract RocketMinipool is RocketMinipoolBase {
+
 
     /*** Libs  *****************/
 
     using SafeMath for uint;
 
 
-    /**** Properties ***********/
-
-    uint256 private calcBase = 1 ether;
-
-    // General
-    uint8   public version = 1;                                         // Version of this contract
-    Status  private status;                                             // The current status of this pool, statuses are declared via Enum in the minipool settings
-    Node    private node;                                               // Node this minipool is attached to, its creator 
-    Staking private staking;                                            // Staking properties of the minipool to track
-    uint256 private userDepositCapacity;                                // Total capacity for user deposits
-    uint256 private userDepositTotal;                                   // Total value of all assigned user deposits
-    uint256 private stakingUserDepositsWithdrawn;                       // Total value of user deposits withdrawn while staking
-    mapping (bytes32 => StakingWithdrawal) private stakingWithdrawals;  // Information on deposit withdrawals made by users while staking
-    bytes32[] private stakingWithdrawalIDs;
-
-    // Deposits
-    mapping (bytes32 => Deposit) private deposits;              // Deposits in this pool
-    bytes32[] private depositIDs;                               // IDs of deposits in this pool for iteration
-
-
-    /*** Contracts **************/
-
-    ERC20 rplContract = ERC20(0);                                                                   // The address of our RPL ERC20 token contract
-    ERC20 rpbContract = ERC20(0);                                                                   // The address of our RPB ERC20 token contract
-    DepositInterface casperDeposit = DepositInterface(0);                                           // Interface of the Casper deposit contract
-    RocketGroupContractInterface rocketGroupContract = RocketGroupContractInterface(0);             // The users group contract that they belong to
-    RocketNodeContractInterface rocketNodeContract = RocketNodeContractInterface(0);                // The node contract for the node which owns this minipool
-    RocketNodeSettingsInterface rocketNodeSettings = RocketNodeSettingsInterface(0);                // The settings for nodes
-    RocketPoolInterface rocketPool = RocketPoolInterface(0);                                        // The main pool manager
-    RocketMinipoolSettingsInterface rocketMinipoolSettings = RocketMinipoolSettingsInterface(0);    // The main settings contract most global parameters are maintained
-    RocketStorageInterface rocketStorage = RocketStorageInterface(0);                               // The main Rocket Pool storage contract where primary persistant storage is maintained
-    PublisherInterface publisher = PublisherInterface(0);                                           // Main pubsub system event publisher
-
-    
-    /*** Structs ***************/
-
-    struct Status {
-        uint8   current;                                        // The current status code, see RocketMinipoolSettings for more information
-        uint8   previous;                                       // The previous status code
-        uint256 time;                                           // The time the status last changed
-        uint256 block;                                          // The block number the status last changed
-    }
-
-    struct Node {
-        address owner;                                          // Etherbase address of the node which owns this minipool
-        address contractAddress;                                // The nodes Rocket Pool contract
-        uint256 depositEther;                                   // The nodes required ether contribution
-        uint256 depositRPL;                                     // The nodes required RPL contribution
-        bool    trusted;                                        // Was the node trusted at the time of minipool creation?
-        bool    depositExists;                                  // The node operator's deposit exists
-        uint256 balance;                                        // The node operator's ether balance
-        uint256 userFee;                                        // The fee charged to users by the node, determined when the minipool begins staking
-    }
-
-    struct Staking {
-        string  id;                                             // Duration ID
-        uint256 duration;                                       // Duration in blocks
-        uint256 balanceStart;                                   // Ether balance of this minipool when it begins staking
-        uint256 balanceEnd;                                     // Ether balance of this minipool when it completes staking
-        bytes   depositInput;                                   // DepositInput data to be submitted to the casper deposit contract
-    }
-
-    struct Deposit {
-        address userID;                                         // Address ID of the user
-        address groupID;                                        // Address ID of the user's group
-        uint256 balance;                                        // Chunk balance deposited
-        uint256 stakingTokensWithdrawn;                         // RPB tokens withdrawn by the user during staking
-        uint256 feeRP;                                          // Rocket Pool's fee
-        uint256 feeGroup;                                       // Group fee
-        uint256 created;                                        // Creation timestamp
-        bool    exists;                                         // Deposit exists
-        uint256 idIndex;                                        // Deposit's index in the ID list
-    }
-
-    struct StakingWithdrawal {
-        address groupFeeAddress;                                // The address to send group fees to
-        uint256 amount;                                         // The amount withdrawn by the user
-        uint256 feeRP;                                          // The fee charged to the user by Rocket Pool
-        uint256 feeGroup;                                       // The fee charged to the user by the group
-        bool exists;
-    }
-
-
-      
     /*** Events ****************/
+
 
     event DepositReceived (
         address indexed _fromAddress,                           // From address
         uint256 amount,                                         // Amount of the deposit
         uint256 created                                         // Creation timestamp
     );
-
-
-
-    /*** Modifiers *************/
-
-
-    /// @dev Only the node contract which this minipool belongs to
-    /// @param _nodeContract The node contract address
-    modifier isNodeContract(address _nodeContract) {
-        require(_nodeContract != address(0x0) && _nodeContract == node.contractAddress, "Incorrect node contract address passed.");
-        _;
-    }
-
-    /// @dev Only allow access from the latest version of the specified Rocket Pool contract
-    modifier onlyLatestContract(string memory _contract) {
-        require(msg.sender == getContractAddress(_contract), "Only the latest specified Rocket Pool contract can access this method.");
-        _;
-    }
-
 
 
     /*** Methods *************/
@@ -148,16 +40,9 @@ contract RocketMinipool {
     /// @param _depositEther Ether amount deposited by the node owner
     /// @param _depositRPL RPL amount deposited by the node owner
     /// @param _trusted Is the node trusted at the time of minipool creation?
-    constructor(address _rocketStorageAddress, address _nodeOwner, string memory _durationID, bytes memory _depositInput, uint256 _depositEther, uint256 _depositRPL, bool _trusted) public {
-        // Update the storage contract address
-        rocketStorage = RocketStorageInterface(_rocketStorageAddress);
+    constructor(address _rocketStorageAddress, address _nodeOwner, string memory _durationID, bytes memory _depositInput, uint256 _depositEther, uint256 _depositRPL, bool _trusted) RocketMinipoolBase(_rocketStorageAddress) public {
         // Get minipool settings
         rocketMinipoolSettings = RocketMinipoolSettingsInterface(getContractAddress("rocketMinipoolSettings"));
-        // Set the address of the casper deposit contract
-        casperDeposit = DepositInterface(getContractAddress("casperDeposit"));
-        // Add the token contract addresses
-        rplContract = ERC20(getContractAddress("rocketPoolToken"));
-        rpbContract = ERC20(getContractAddress("rocketBETHToken"));
         // Set the initial status
         status.current = 0;
         status.time = now;
@@ -185,15 +70,6 @@ contract RocketMinipool {
     function() external payable { 
         // Log the deposit received
         emit DepositReceived(msg.sender, msg.value, now);       
-    }
-
-
-    // Utility Methods
-
-    /// @dev Get the the contracts address - This method should be called before interacting with any RP contracts to ensure the latest address is used
-    function getContractAddress(string memory _contractName) private view returns(address) { 
-        // Get the current API contract address 
-        return rocketStorage.getAddress(keccak256(abi.encodePacked("contract.name", _contractName)));
     }
 
 
