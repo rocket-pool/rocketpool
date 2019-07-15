dbd9929c2bfe725dbf1092e0259751121e0629c8
Jake Pospischil <moles1@gmail.com>
2019-07-15 14:40:13 +1000

    added deposit helper methods

--- test/_helpers/rocket-deposit.js
+++ test/_helpers/rocket-deposit.js
@@ -12,8 +12,23 @@ export async function getQueuedDepositIDs({groupID, userID, durationID}) {
 }
 
 
+// Get all of a user's deposit IDs
+export async function getDepositIDs({groupID, userID, durationID}) {
+	const rocketDepositIndex = await RocketDepositIndex.deployed();
+	let depositCount = parseInt(await rocketDepositIndex.getUserDepositCount.call(groupID, userID, durationID));
+    let depositIDs = [], di;
+    for (di = 0; di < depositCount; ++di) depositIDs.push(await rocketDepositIndex.getUserDepositAt.call(groupID, userID, durationID, di));
+    return depositIDs;
+}
+
+
 // Make a user deposit
 export async function userDeposit({depositorContract, durationID, fromAddress, value}) {
     await depositorContract.deposit(durationID, {from: fromAddress, value: value});
+}
+
+// Withdraw a user deposit from a withdrawn minipool
+export async function userWithdrawMinipoolDeposit({withdrawerContract, depositID, minipoolAddress, fromAddress}) {
+	await withdrawerContract.depositWithdrawMinipool(depositID, minipoolAddress, {from: fromAddress});
 }
 
