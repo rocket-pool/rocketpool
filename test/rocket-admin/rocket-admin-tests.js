c277aa2c3f4b30f1d91e813c556fbf9e878e6e8a
Jake Pospischil <moles1@gmail.com>
2019-07-23 15:43:20 +1000

    implemented RocketAdmin.setGroupRocketPoolFeePercent unit tests

--- test/rocket-admin/rocket-admin-tests.js
+++ test/rocket-admin/rocket-admin-tests.js
@@ -1,6 +1,8 @@
 import { printTitle, assertThrows } from '../_lib/utils/general';
+import { RocketGroupSettings } from '../_lib/artifacts';
 import { createNodeContract } from '../_helpers/rocket-node';
-import { scenarioSetNodeTrusted } from './rocket-admin-scenarios';
+import { createGroupContract } from '../_helpers/rocket-group';
+import { scenarioSetNodeTrusted, scenarioSetGroupRocketPoolFeePercent } from './rocket-admin-scenarios';
 
 export default function() {
 
@@ -10,13 +12,18 @@ export default function() {
         // Accounts
         const owner = accounts[0];
         const nodeOperator = accounts[1];
+        const groupOwner = accounts[2];
 
 
         // Setup
+        let groupContract;
         before(async () => {
 
             // Create node contract
             let nodeContract = await createNodeContract({timezone: 'Australia/Brisbane', nodeOperator});
+
+            // Create group contract
+            groupContract = await createGroupContract({name: 'Group 1', stakingFee: web3.utils.toWei('0.05', 'ether'), groupOwner});
 
         });
 
@@ -84,6 +91,61 @@ export default function() {
                 fromAddress: accounts[9],
                 gas: 5000000,
             }), 'Random account set a node\'s trusted status');
+        });
+
+
+        // Owner can set a group's RP fee percentage
+        it(printTitle('owner', 'can set a group\'s RP fee percentage'), async () => {
+            await scenarioSetGroupRocketPoolFeePercent({
+                groupId: groupContract.address,
+                feePerc: web3.utils.toWei('0.01', 'ether'),
+                fromAddress: owner,
+                gas: 5000000,
+            });
+        });
+
+
+        // Owner cannot set the RP fee percentage for an invalid group
+        it(printTitle('owner', 'cannot set the RP fee percentage for an invalid group'), async () => {
+            await assertThrows(scenarioSetGroupRocketPoolFeePercent({
+                groupId: accounts[9],
+                feePerc: web3.utils.toWei('0.01', 'ether'),
+                fromAddress: owner,
+                gas: 5000000,
+            }), 'Set the RP fee percentage for an invalid group');
+        });
+
+
+        // Owner cannot set a group's RP fee percentage above the maximum
+        it(printTitle('owner', 'cannot set a group\'s RP fee percentage above the maximum'), async () => {
+            const rocketGroupSettings = await RocketGroupSettings.deployed();
+
+            // Get maximum fee percentage
+            let maxFee = parseInt(await rocketGroupSettings.getMaxFee.call());
+
+            // Get value to set
+            let value = web3.utils.toWei('0.9', 'ether');
+            assert.isTrue(parseInt(value) > maxFee, 'Pre-check failed: value is below the maximum fee limit');
+
+            // Set group RP fee percentage
+            await assertThrows(scenarioSetGroupRocketPoolFeePercent({
+                groupId: groupContract.address,
+                feePerc: value,
+                fromAddress: owner,
+                gas: 5000000,
+            }), 'Set the RP fee percentage for a group above the maximum');
+
+        });
+
+
+        // Random account cannot set a group's RP fee percentage
+        it(printTitle('random account', 'cannot set a group\'s RP fee percentage'), async () => {
+            await assertThrows(scenarioSetGroupRocketPoolFeePercent({
+                groupId: groupContract.address,
+                feePerc: web3.utils.toWei('0.01', 'ether'),
+                fromAddress: accounts[9],
+                gas: 5000000,
+            }), 'Random account set a group\'s RP fee percentage');
         });
 
 
