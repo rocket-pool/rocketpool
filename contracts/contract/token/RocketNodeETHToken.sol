21ac575542b2f7d85eb49f3bc634ec8be95e28df
Jake Pospischil <moles1@gmail.com>
2020-05-25 15:37:12 +1000

    added nETH token

---
+++ contracts/contract/token/RocketNodeETHToken.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.6.8;
+
+// nETH is paid to node operators when their eth 2.0 validators become withdrawable
+// nETH is backed by ETH (subject to liquidity) 1:1
+// nETH will be replaced by direct BETH payments after eth 2.0 phase 2
+
+contract RocketNodeETHToken {
+
+    // Various ERC20 methods
+
+    // Mint nETH
+    // Only accepts calls from registered minipools
+    function mint(uint256 _amount, address _to) public {}
+
+    // Burn nETH for ETH
+    function burn(uint256 _amount) public {
+        // 1. Check contract ETH balance
+        // 2. Decrease total supply and account balance
+        // 3. Transfer ETH to account
+    }
+
+}
