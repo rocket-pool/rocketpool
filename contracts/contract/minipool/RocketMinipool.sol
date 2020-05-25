74f22e8d25867cb75d78513ffe6cadf6d1f23b7b
Jake Pospischil <moles1@gmail.com>
2020-05-25 14:39:36 +1000

    moved watchtower functions to relevant contracts

--- contracts/contract/minipool/RocketMinipool.sol
+++ contracts/contract/minipool/RocketMinipool.sol
@@ -9,11 +9,11 @@ contract RocketMinipool {
     function stakeMinipool(bytes memory _validatorPubkey, bytes memory _validatorSignature, bytes32 _depositDataRoot) public {}
 
     // Mark the minipool as exited
-    // Only accepts calls from the RocketNodeWatchtower contract
+    // Only accepts calls from trusted (oracle) nodes
     function exitMinipool() public {}
 
     // Mark the minipool as withdrawable and record its final balance
-    // Only accepts calls from the RocketNodeWatchtower contract
+    // Only accepts calls from trusted (oracle) nodes
     function withdrawMinipool(uint256 _withdrawalBalance) public {}
 
     // Withdraw rewards from the minipool and close it
