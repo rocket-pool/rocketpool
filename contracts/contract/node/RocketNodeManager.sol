23417d709cae43ac75f603a0927717c0e94c8325
Jake Pospischil <moles1@gmail.com>
2020-05-25 15:14:27 +1000

    minor fixes

--- contracts/contract/node/RocketNodeManager.sol
+++ contracts/contract/node/RocketNodeManager.sol
@@ -15,6 +15,9 @@ contract RocketNodeManager {
 
     // Perform a node checkin
     // Only accepts calls from registered nodes
-    function checkin() public {}
+    function checkin() public {
+        // 1. Disable inactive nodes
+        // 2. Update the node reward period
+    }
 
 }
