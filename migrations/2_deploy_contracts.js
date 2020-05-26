a03c43aca9e554b7acb4ace792bd9d8dfe2e8f9b
Jake Pospischil <moles1@gmail.com>
2020-05-26 13:33:09 +1000

    updating deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,121 +1,18 @@
 /*** Dependencies ********************/
+
+
 const pako = require('pako');
 
 
 /*** Settings ************************/
+
+
 const config = require('../truffle.js');
 
 
-// Check if testing utility contracts
-const testScript = process.argv[process.argv.length - 1];
-const testUtils = !!testScript.match(/util/i);
-
-
-/*** Contracts ***********************/
-// Storage
-const rocketStorage = artifacts.require('./RocketStorage.sol');
-// All other contracts
-const contracts = {};
-// ABIs for deployment
-const abis = {};
-// Core
-contracts.rocketAdmin = artifacts.require('./RocketAdmin.sol');
-contracts.rocketPool = artifacts.require('./RocketPool.sol');
-contracts.rocketRole = artifacts.require('./RocketRole.sol');
-contracts.rocketNode = artifacts.require('./RocketNode.sol');
-contracts.rocketPIP = artifacts.require('./RocketPIP.sol');
-contracts.rocketUpgrade = artifacts.require('./RocketUpgrade.sol');
-contracts.rocketUpgradeApproval = artifacts.require('./RocketUpgradeApproval.sol');
-// API
-contracts.rocketDepositAPI = artifacts.require('./api/RocketDepositAPI.sol');
-contracts.rocketGroupAPI = artifacts.require('./api/RocketGroupAPI.sol');
-contracts.rocketNodeAPI = artifacts.require('./api/RocketNodeAPI.sol');
-// Deposit
-contracts.rocketDeposit = artifacts.require('./deposit/RocketDeposit.sol');
-contracts.rocketDepositIndex = artifacts.require('./deposit/RocketDepositIndex.sol');
-contracts.rocketDepositQueue = artifacts.require('./deposit/RocketDepositQueue.sol');
-contracts.rocketDepositVault = artifacts.require('./deposit/RocketDepositVault.sol');
-// Group
-contracts.rocketGroupAccessorFactory = artifacts.require('./group/RocketGroupAccessorFactory.sol');
-// Node
-contracts.rocketNodeFactory = artifacts.require('./node/RocketNodeFactory.sol');
-contracts.rocketNodeKeys = artifacts.require('./node/RocketNodeKeys.sol');
-contracts.rocketNodeTasks = artifacts.require('./node/RocketNodeTasks.sol');
-contracts.rocketNodeWatchtower = artifacts.require('./node/RocketNodeWatchtower.sol');
-// Minipool
-contracts.rocketMinipoolDelegateNode = artifacts.require('./minipool/RocketMinipoolDelegateNode.sol');
-contracts.rocketMinipoolDelegateStatus = artifacts.require('./minipool/RocketMinipoolDelegateStatus.sol');
-contracts.rocketMinipoolDelegateDeposit = artifacts.require('./minipool/RocketMinipoolDelegateDeposit.sol');
-contracts.rocketMinipoolFactory = artifacts.require('./minipool/RocketMinipoolFactory.sol');
-contracts.rocketMinipoolSet = artifacts.require('./minipool/RocketMinipoolSet.sol');
-// Settings
-contracts.rocketMinipoolSettings = artifacts.require('./settings/RocketMinipoolSettings.sol');
-contracts.rocketDepositSettings = artifacts.require('./settings/RocketDepositSettings.sol');
-contracts.rocketGroupSettings = artifacts.require('./settings/RocketGroupSettings.sol');
-contracts.rocketNodeSettings = artifacts.require('./settings/RocketNodeSettings.sol');
-// Dummy Contracts
-contracts.rocketPoolToken = artifacts.require('./token/DummyRocketPoolToken.sol');
-contracts.rocketETHToken = artifacts.require('./token/RocketETHToken.sol');
-// Node tasks
-contracts.taskDisableInactiveNodes = artifacts.require('./tasks/DisableInactiveNodes.sol');
-contracts.taskCalculateNodeFee = artifacts.require('./tasks/CalculateNodeFee.sol');
-// Utilities
-contracts.utilMaths = artifacts.require('./utils/Maths.sol');
-contracts.utilPublisher = artifacts.require('./Publisher.sol');
-contracts.utilAddressQueueStorage = artifacts.require('./AddressQueueStorage.sol');
-contracts.utilBytes32QueueStorage = artifacts.require('./Bytes32QueueStorage.sol');
-contracts.utilAddressSetStorage = artifacts.require('./AddressSetStorage.sol');
-contracts.utilBytes32SetStorage = artifacts.require('./Bytes32SetStorage.sol');
-contracts.utilStringSetStorage = artifacts.require('./StringSetStorage.sol');
-// Extra utilities
-if (testUtils) {
-  contracts.utilAddressListStorage = artifacts.require('./AddressListStorage.sol');
-  contracts.utilBoolListStorage = artifacts.require('./BoolListStorage.sol');
-  contracts.utilBytesListStorage = artifacts.require('./BytesListStorage.sol');
-  contracts.utilBytes32ListStorage = artifacts.require('./Bytes32ListStorage.sol');
-  contracts.utilIntListStorage = artifacts.require('./IntListStorage.sol');
-  contracts.utilStringListStorage = artifacts.require('./StringListStorage.sol');
-  contracts.utilUintListStorage = artifacts.require('./UintListStorage.sol');
-  contracts.utilBoolQueueStorage = artifacts.require('./BoolQueueStorage.sol');
-  contracts.utilBytesQueueStorage = artifacts.require('./BytesQueueStorage.sol');
-  contracts.utilIntQueueStorage = artifacts.require('./IntQueueStorage.sol');
-  contracts.utilStringQueueStorage = artifacts.require('./StringQueueStorage.sol');
-  contracts.utilUintQueueStorage = artifacts.require('./UintQueueStorage.sol');
-  contracts.utilBoolSetStorage = artifacts.require('./BoolSetStorage.sol');
-  contracts.utilBytesSetStorage = artifacts.require('./BytesSetStorage.sol');
-  contracts.utilIntSetStorage = artifacts.require('./IntSetStorage.sol');
-  contracts.utilUintSetStorage = artifacts.require('./UintSetStorage.sol');
-}
-
-// ABIs
-abis.rocketGroupContract = artifacts.require('./group/RocketGroupContract.sol');
-abis.rocketGroupAccessorContract = artifacts.require('./group/RocketGroupAccessorContract.sol');
-abis.rocketNodeContract = artifacts.require('./node/RocketNodeContract.sol');
-abis.rocketMinipool = artifacts.require('./minipool/RocketMinipool.sol');
-
-// Minipool staking durations
-const stakingDurations = {
-  '3m': 20250,
-  '6m': 40500,
-  '12m': 81000,
-};
-
-// Pubsub event subscriptions
-const subscriptions = {
-  'minipool.status.change': ['rocketPool'],
-  'minipool.user.deposit': ['rocketPool'],
-  'minipool.available.change': ['rocketNode', 'rocketMinipoolSet'],
-  'node.active.change': ['rocketNode'],
-};
-
-// Node tasks to register
-const nodeTasks = {
-  'DisableInactiveNodes': contracts.taskDisableInactiveNodes,
-  'CalculateNodeFee': contracts.taskCalculateNodeFee,
-};
-
-
 /*** Utility Methods *****************/
+
+
 // Compress / decompress ABIs
 function compressABI(abi) {
   return Buffer.from(pako.deflate(JSON.stringify(abi))).toString('base64');
@@ -123,13 +20,47 @@ function compressABI(abi) {
 function decompressABI(abi) {
   return JSON.parse(pako.inflate(Buffer.from(abi, 'base64'), {to: 'string'}));
 }
+
 // Load ABI files and parse
 function loadABI(abiFilePath) {
   return JSON.parse(config.fs.readFileSync(abiFilePath));
 }
 
 
-// Start exporting now
+/*** Contracts ***********************/
+
+
+// Storage
+const rocketStorage = artifacts.require('./RocketStorage.sol');
+
+// Network contracts
+const contracts = {
+  // Core
+  rocketPool:             artifacts.require('./RocketPool.sol'),
+  rocketVault:            artifacts.require('./RocketVault.sol'),
+  // Deposit
+  rocketDepositPool:      artifacts.require('./RocketDepositPool.sol'),
+  // Minipool
+  rocketMinipoolManager:  artifacts.require('./RocketMinipoolManager.sol'),
+  // Node
+  rocketNodeManager:      artifacts.require('./RocketNodeManager.sol'),
+  rocketNodeRewards:      artifacts.require('./RocketNodeRewards.sol'),
+  // Tokens
+  rocketETHToken:         artifacts.require('./RocketETHToken.sol'),
+  rocketNodeETHToken:     artifacts.require('./RocketNodeETHToken.sol'),
+};
+
+// Instance contract ABIs
+const abis = {
+  rocketMinipool:         artifacts.require('./RocketMinipool.sol'),
+  rocketNode:             artifacts.require('./RocketNode.sol'),
+};
+
+
+/*** Deployment **********************/
+
+
+// Deploy Rocket Pool
 module.exports = async (deployer, network) => {
 
   // Truffle add '-fork' for some reason when deploying to actual testnets
@@ -149,8 +80,7 @@ module.exports = async (deployer, network) => {
     return result;
   });
 
-  
-  
+
   // Live deployment
   if ( network == 'live' ) {
     // Casper live contract address
@@ -204,16 +134,9 @@ module.exports = async (deployer, network) => {
       precompiled: true
     };
 
-    // Test interface contracts
-    if (testUtils) {
-      contracts.testLists = artifacts.require('./test/TestLists.sol');
-      contracts.testQueues = artifacts.require('./test/TestQueues.sol');
-      contracts.testSets = artifacts.require('./test/TestSets.sol');
-    }
-
   }
- 
-  
+
+
   // Deploy rocketStorage first - has to be done in this order so that the following contracts already know the storage address
   await deployer.deploy(rocketStorage);
   // Update the storage with the new addresses
@@ -227,9 +150,9 @@ module.exports = async (deployer, network) => {
       }
     }
   };
-  
   // Run it
   await deployContracts();
+
   // Register all other contracts with storage and store their abi
   const addContracts = async function() {
     // Log RocketStorage
@@ -258,7 +181,7 @@ module.exports = async (deployer, network) => {
       } 
     }
   };
-  
+
   // Register ABI-only contracts
   const addABIs = async function() {
     for (let contract in abis) {
@@ -273,110 +196,7 @@ module.exports = async (deployer, network) => {
       }
     }
   };
-  
-  // Register staking durations
-  const registerStakingDurations = async function() {
-    let minipoolSettingsInstance = await contracts.rocketMinipoolSettings.deployed();
-    for (let duration in stakingDurations) {
-      // Log it
-      console.log('\x1b[31m%s\x1b[0m:', '   Register staking duration');
-      console.log('     '+duration+': '+stakingDurations[duration]);
-      await minipoolSettingsInstance.addMinipoolStakingDuration(duration, stakingDurations[duration]);
-    }
-  }
-  
-  // Register uniswap contracts
-  const addUniswap = async function() {
-    if (network != 'live') {
 
-      // Load contract data
-      const FactoryABI = loadABI('./contracts/contract/uniswap/compiled/factory.abi');
-      const FactoryBytecode = config.fs.readFileSync('./contracts/contract/uniswap/compiled/factory.bin');
-      const ExchangeABI = loadABI('./contracts/contract/uniswap/compiled/exchange.abi');
-      const ExchangeBytecode = config.fs.readFileSync('./contracts/contract/uniswap/compiled/exchange.bin');
-
-      // Get RP token contract addresses
-      let rplAddress = contracts.rocketPoolToken.address;
-      let rethAddress = contracts.rocketETHToken.address;
-
-      // Initialise contracts
-      let ExchangeContract = new $web3.eth.Contract(ExchangeABI);
-      let FactoryContract = new $web3.eth.Contract(FactoryABI);
-
-      // Deploy contracts
-      let exchange = await ExchangeContract.deploy({data: ExchangeBytecode}).send({from: accounts[0], gas: 8000000});
-      let factory = await FactoryContract.deploy({data: FactoryBytecode}).send({from: accounts[0], gas: 8000000});
-
-      // Initialise factory
-      await factory.methods.initializeFactory(exchange.options.address).send({from: accounts[0], gas: 8000000});
-
-      // Create token exchanges
-      await factory.methods.createExchange(rplAddress).send({from: accounts[0], gas: 8000000});
-      await factory.methods.createExchange(rethAddress).send({from: accounts[0], gas: 8000000});
-
-      // Get exchange addresses
-      let rplExchangeAddress = await factory.methods.getExchange(rplAddress).call();
-      let rethExchangeAddress = await factory.methods.getExchange(rethAddress).call();
-
-      // Log
-      console.log('\x1b[31m%s\x1b[0m:', '   Uniswap factory address');
-      console.log('     ' + factory.options.address);
-      console.log('\x1b[31m%s\x1b[0m:', '   Uniswap RPL exchange address');
-      console.log('     ' + rplExchangeAddress);
-      console.log('\x1b[31m%s\x1b[0m:', '   Uniswap rETH exchange address');
-      console.log('     ' + rethExchangeAddress);
-
-    }
-  };
-  
-  // Register event subscriptions
-  const registerSubscriptions = async function() {
-    let publisherInstance = await contracts.utilPublisher.deployed();
-    for (let event in subscriptions) {
-      // Log it
-      console.log('\x1b[31m%s\x1b[0m:', '   Add subscription event '+event);
-      for (let si = 0; si < subscriptions[event].length; ++si) {
-        console.log('     '+subscriptions[event][si]);
-        // Regsiter the subscription
-        await publisherInstance.addSubscriber($web3.utils.soliditySha3(event), subscriptions[event][si]);
-      }
-    }
-  }
-  
-  // Register node tasks
-  const registerNodeTasks = async function() {
-    let nodeTasksInstance = await contracts.rocketNodeTasks.deployed();
-    for (let task in nodeTasks) {
-      // Log it
-      console.log('\x1b[31m%s\x1b[0m:', '   Register node task');
-      console.log('     '+task);
-      let taskInstance = await nodeTasks[task].deployed();
-      await nodeTasksInstance.add(taskInstance.address);
-    }
-  }
-  // Deploy default Rocket Pool group & accessor contract
-  const registerRocketPoolGroup = async function() {
-    // Get contracts
-    let rocketGroupSettingsInstance = await contracts.rocketGroupSettings.deployed();
-    let rocketGroupAPIInstance = await contracts.rocketGroupAPI.deployed();
-    // Get new group fee
-    let newGroupFee = await rocketGroupSettingsInstance.getNewFee.call();
-    // Create group
-    let groupAddResult = await rocketGroupAPIInstance.add('rocketpool', 0, {value: newGroupFee});
-    let groupId = groupAddResult.logs.filter(log => log.event == 'GroupAdd')[0].args.ID;
-    // Create group accessor
-    let groupAccessorResult = await rocketGroupAPIInstance.createDefaultAccessor(groupId);
-    let groupAccessorAddress = groupAccessorResult.logs.filter(log => log.event == 'GroupCreateDefaultAccessor')[0].args.accessorAddress;
-    // Add group accessor
-    let rocketGroupInstance = await abis.rocketGroupContract.at(groupId);
-    await rocketGroupInstance.addDepositor(groupAccessorAddress);
-    await rocketGroupInstance.addWithdrawer(groupAccessorAddress);
-    // Log it
-    console.log('\x1b[31m%s\x1b[0m:', '   Rocket Pool group address');
-    console.log('     '+groupId);
-    console.log('\x1b[31m%s\x1b[0m:', '   Rocket Pool group accessor address');
-    console.log('     '+groupAccessorAddress);
-  }
   // Run it
   console.log('  Deploy Contracts');
   console.log('\x1b[34m%s\x1b[0m', '  ******************************************');
@@ -385,26 +205,7 @@ module.exports = async (deployer, network) => {
   console.log('\x1b[34m%s\x1b[0m', '  Set ABI Only Storage');
   console.log('\x1b[34m%s\x1b[0m', '  ******************************************');
   await addABIs();
-  console.log('\n');
-  console.log('\x1b[34m%s\x1b[0m', '  Register Staking Durations');
-  console.log('\x1b[34m%s\x1b[0m', '  ******************************************');
-  await registerStakingDurations();
-  console.log('\n');
-  console.log('\x1b[34m%s\x1b[0m', '  Deploy Uniswap contracts');
-  console.log('\x1b[34m%s\x1b[0m', '  ******************************************');
-  await addUniswap();
-  console.log('\n');
-  console.log('\x1b[34m%s\x1b[0m', '  Register Event Subscribers');
-  console.log('\x1b[34m%s\x1b[0m', '  ******************************************');
-  await registerSubscriptions();
-  console.log('\n');
-  console.log('\x1b[34m%s\x1b[0m', '  Register Node Tasks');
-  console.log('\x1b[34m%s\x1b[0m', '  ******************************************');
-  await registerNodeTasks();
-  console.log('\n');
-  console.log('\x1b[34m%s\x1b[0m', '  Register RP Group');
-  console.log('\x1b[34m%s\x1b[0m', '  ******************************************');
-  await registerRocketPoolGroup();
+
   // Disable direct access to storage now
   await rocketStorageInstance.setBool(
     $web3.utils.soliditySha3('contract.storage.initialised'),
@@ -416,5 +217,4 @@ module.exports = async (deployer, network) => {
   console.log('\n');  
 
 };
-
 
