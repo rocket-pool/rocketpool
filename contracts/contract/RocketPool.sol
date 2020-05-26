a27cae1912649166b54df1f176251075115296d5
Jake Pospischil <moles1@gmail.com>
2020-05-27 09:33:27 +1000

    added validator pubkey to beacon withdrawals endpoint for logging

--- contracts/contract/RocketPool.sol
+++ contracts/contract/RocketPool.sol
@@ -50,7 +50,7 @@ contract RocketPool is RocketBase {
 
     // Process a validator withdrawal from the beacon chain
     // Only accepts calls from trusted (withdrawer) nodes (TBA)
-    function beaconWithdrawal() public {
+    function beaconWithdrawal(bytes memory _validatorPubkey) public {
         // 1. Calculate the share of the validator balance for node operators vs users
         // 2. Transfer the node operators' share to the nETH contract
         // 3. Transfer the users' share:
