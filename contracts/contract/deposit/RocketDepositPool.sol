5916698cc414bf07a95d93d58490762a905d81de
Jake Pospischil <moles1@gmail.com>
2020-05-27 14:52:40 +1000

    implemented user deposit

--- contracts/contract/deposit/RocketDepositPool.sol
+++ contracts/contract/deposit/RocketDepositPool.sol
@@ -3,6 +3,12 @@ pragma solidity 0.6.8;
 // SPDX-License-Identifier: GPL-3.0-only
 
 import "../RocketBase.sol";
+import "../../interface/RocketPoolInterface.sol";
+import "../../interface/RocketVaultInterface.sol";
+import "../../interface/node/RocketNodeRewardsInterface.sol";
+import "../../interface/settings/RocketDepositSettingsInterface.sol";
+import "../../interface/token/RocketETHTokenInterface.sol";
+import "../../lib/SafeMath.sol";
 
 // The main entry point for deposits into the RP network
 // Accepts user deposits and mints rETH; fees are deducted and the remainder is sent to the user
@@ -10,20 +16,63 @@ import "../RocketBase.sol";
 
 contract RocketDepositPool is RocketBase {
 
+    // Libs
+    using SafeMath for uint;
+
     // Construct
     constructor(address _rocketStorageAddress) RocketBase(_rocketStorageAddress) public {
         version = 1;
     }
 
+    // Current deposit pool balance
+    function getBalance() public view returns (uint256) {
+        return rocketStorage.getUint(keccak256(abi.encodePacked("deposit.pool.balance")));
+    }
+    function setBalance(uint256 _value) private {
+        rocketStorage.setUint(keccak256(abi.encodePacked("deposit.pool.balance")), _value);
+    }
+
     // Accept a deposit from a user
     // The user specifies the maximum fee % they are willing to pay as a fraction of 1 ETH
     function deposit(uint256 _maxFee) public payable {
-        // 1. Check the current network fee does not exceed the max fee specified
-        // 2. Mint rETH equivalent to the deposit amount
-        // 3. Update the RP network total ETH balance
-        // 4. Transfer ETH & fee to the vault
-        // 5. Transfer remaining rETH to the user
-        // 6. Assign deposits
+        // Calculation base value
+        uint256 calcBase = 1 ether;
+        // Load contracts
+        RocketDepositSettingsInterface rocketDepositSettings = RocketDepositSettingsInterface(getContractAddress("rocketDepositSettings"));
+        RocketETHTokenInterface rocketETHToken = RocketETHTokenInterface(getContractAddress("rocketETHToken"));
+        RocketNodeRewardsInterface rocketNodeRewards = RocketNodeRewardsInterface(getContractAddress("rocketNodeRewards"));
+        RocketPoolInterface rocketPool = RocketPoolInterface(getContractAddress("rocketPool"));
+        RocketVaultInterface rocketVault = RocketVaultInterface(getContractAddress("rocketVault"));
+        // Check deposit settings
+        require(rocketDepositSettings.getDepositEnabled(), "Deposits into Rocket Pool are currently disabled");
+        require(msg.value >= rocketDepositSettings.getMinimumDeposit(), "The deposited amount is less than the minimum deposit size");
+        // Update deposit pool balance to include deposited amount
+        setBalance(getBalance().add(msg.value));
+        // Get current deposit fee
+        uint256 depositFee = rocketPool.getDepositFee();
+        // Check current deposit fee against max specified
+        require(depositFee <= _maxFee, "The current network deposit fee exceeds the maximum fee specified");
+        // Calculate amount of rETH to mint
+        uint256 rethExchangeRate = rocketETHToken.getExchangeRate();
+        uint256 rethAmount;
+        if (rethExchangeRate == 0) { rethAmount = msg.value; }
+        else { rethAmount = calcBase.mul(msg.value).div(rethExchangeRate); }
+        // Mint rETH here
+        rocketETHToken.mint(rethAmount, address(this));
+        // Update network ETH balance
+        rocketPool.setTotalETHBalance(rocketPool.getTotalETHBalance().add(msg.value));
+        // Calculate deposit fee amount and user share of rETH
+        uint256 feeAmount = rethAmount.mul(depositFee).div(calcBase);
+        uint256 userAmount = rethAmount.sub(feeAmount);
+        // Transfer rETH to vault & user
+        require(rocketETHToken.transfer(address(rocketVault), feeAmount), "rETH was not transferred to the vault successfully");
+        require(rocketETHToken.transfer(msg.sender, userAmount), "rETH was not transferred to the user successfully");
+        // Update node reward pool balance
+        rocketNodeRewards.setBalance(rocketNodeRewards.getBalance().add(feeAmount));
+        // Transfer ETH to vault
+        rocketVault.depositEther{value: msg.value}();
+        // Assign deposits
+        assignDeposits();
     }
 
     // Recycle a deposit from a withdrawn minipool
