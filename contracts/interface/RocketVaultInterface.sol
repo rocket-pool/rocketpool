72c774a51de1f51b8ffd5fcb64dfb969d390d98a
Jake Pospischil <moles1@gmail.com>
2020-05-27 13:58:39 +1000

    vault interface

---
+++ contracts/interface/RocketVaultInterface.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.6.8;
+
+// SPDX-License-Identifier: GPL-3.0-only
+
+interface RocketVaultInterface {
+    function depositEther() external payable;
+    function withdrawEther(address _withdrawalAddress, uint256 _amount) external;
+    function withdrawReth(address _withdrawalAddress, uint256 _amount) external;
+}
