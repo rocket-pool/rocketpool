eae5ecb48fc05d3b70251e90cd86939b2d9cfc8c
Jake Pospischil <moles1@gmail.com>
2020-06-05 13:26:26 +1000

    added set & queue storage helpers

--- contracts/contract/util/AddressSetStorage.sol
+++ contracts/contract/util/AddressSetStorage.sol
@@ -16,44 +16,44 @@ contract AddressSetStorage is RocketBase, AddressSetStorageInterface {
 
     // The number of items in a set
     function getCount(bytes32 _key) override external view returns (uint) {
-        return getUint(keccak256(abi.encodePacked(_key, "count")));
+        return getUint(keccak256(abi.encodePacked(_key, ".count")));
     }
 
     // The item in a set by index
     function getItem(bytes32 _key, uint _index) override external view returns (address) {
-        return getAddress(keccak256(abi.encodePacked(_key, "item", _index)));
+        return getAddress(keccak256(abi.encodePacked(_key, ".item", _index)));
     }
 
     // The index of an item in a set
     // Returns -1 if the value is not found
     function getIndexOf(bytes32 _key, address _value) override external view returns (int) {
-        return int(getUint(keccak256(abi.encodePacked(_key, "index", _value)))) - 1;
+        return int(getUint(keccak256(abi.encodePacked(_key, ".index", _value)))) - 1;
     }
 
     // Add an item to a set
     // Requires that the item does not exist in the set
     function addItem(bytes32 _key, address _value) override external onlyLatestNetworkContract {
-        require(getUint(keccak256(abi.encodePacked(_key, "index", _value))) == 0, "Item already exists in set");
-        uint count = getUint(keccak256(abi.encodePacked(_key, "count")));
-        setAddress(keccak256(abi.encodePacked(_key, "item", count)), _value);
-        setUint(keccak256(abi.encodePacked(_key, "index", _value)), count + 1);
-        setUint(keccak256(abi.encodePacked(_key, "count")), count + 1);
+        require(getUint(keccak256(abi.encodePacked(_key, ".index", _value))) == 0, "Item already exists in set");
+        uint count = getUint(keccak256(abi.encodePacked(_key, ".count")));
+        setAddress(keccak256(abi.encodePacked(_key, ".item", count)), _value);
+        setUint(keccak256(abi.encodePacked(_key, ".index", _value)), count + 1);
+        setUint(keccak256(abi.encodePacked(_key, ".count")), count + 1);
     }
 
     // Remove an item from a set
     // Swaps the item with the last item in the set and truncates it; computationally cheap
     // Requires that the item exists in the set
     function removeItem(bytes32 _key, address _value) override external onlyLatestNetworkContract {
-        uint256 index = getUint(keccak256(abi.encodePacked(_key, "index", _value)));
+        uint256 index = getUint(keccak256(abi.encodePacked(_key, ".index", _value)));
         require(index-- > 0, "Item does not exist in set");
-        uint count = getUint(keccak256(abi.encodePacked(_key, "count")));
+        uint count = getUint(keccak256(abi.encodePacked(_key, ".count")));
         if (index < count - 1) {
-            address lastItem = getAddress(keccak256(abi.encodePacked(_key, "item", count - 1)));
-            setAddress(keccak256(abi.encodePacked(_key, "item", index)), lastItem);
-            setUint(keccak256(abi.encodePacked(_key, "index", lastItem)), index + 1);
+            address lastItem = getAddress(keccak256(abi.encodePacked(_key, ".item", count - 1)));
+            setAddress(keccak256(abi.encodePacked(_key, ".item", index)), lastItem);
+            setUint(keccak256(abi.encodePacked(_key, ".index", lastItem)), index + 1);
         }
-        setUint(keccak256(abi.encodePacked(_key, "index", _value)), 0);
-        setUint(keccak256(abi.encodePacked(_key, "count")), count - 1);
+        setUint(keccak256(abi.encodePacked(_key, ".index", _value)), 0);
+        setUint(keccak256(abi.encodePacked(_key, ".count")), count - 1);
     }
 
 }
