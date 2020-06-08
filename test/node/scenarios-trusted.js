298af713a73de5153249c45a14621d6395ef7b8d
Jake Pospischil <moles1@gmail.com>
2020-06-08 14:31:20 +1000

    updated node trusted status tests

--- test/node/scenarios-trusted.js
+++ test/node/scenarios-trusted.js
@@ -7,8 +7,18 @@ export async function setNodeTrusted(nodeAddress, trusted, txOptions) {
     // Load contracts
     const rocketNodeManager = await RocketNodeManager.deployed();
 
+    // Get initial trusted status
+    let trusted1 = await rocketNodeManager.getNodeTrusted.call(nodeAddress);
+
     // Set trusted status
     await rocketNodeManager.setNodeTrusted(nodeAddress, trusted, txOptions);
+
+    // Get updated trusted status
+    let trusted2 = await rocketNodeManager.getNodeTrusted.call(nodeAddress);
+
+    // Check statuses
+    assert.notEqual(trusted1, trusted, 'Incorrect initial trusted status');
+    assert.equal(trusted2, trusted, 'Incorrect updated trusted status');
 
 }
 
