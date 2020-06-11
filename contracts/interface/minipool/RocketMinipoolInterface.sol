dd87b38510ba21552028e859503004eae0ba68a0
Jake Pospischil <moles1@gmail.com>
2020-06-11 13:42:00 +1000

    minor cleanup

--- contracts/interface/minipool/RocketMinipoolInterface.sol
+++ contracts/interface/minipool/RocketMinipoolInterface.sol
@@ -5,9 +5,6 @@ pragma solidity 0.6.8;
 interface RocketMinipoolInterface {
     function nodeDeposit() external payable;
     function userDeposit() external payable;
-    function stake(bytes calldata _validatorPubkey, bytes calldata _validatorSignature, bytes32 _depositDataRoot) external;
     function exit() external;
     function withdraw(uint256 _withdrawalBalance) external;
-    function close() external;
-    function dissolve() external;
 }
