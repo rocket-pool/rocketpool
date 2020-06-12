902f2797ff33e02ed55f5807e636914198298da9
Jake Pospischil <moles1@gmail.com>
2020-06-12 15:46:25 +1000

    implemented network node demand calculation

--- contracts/contract/network/RocketNetworkFees.sol
+++ contracts/contract/network/RocketNetworkFees.sol
@@ -3,6 +3,8 @@ pragma solidity 0.6.8;
 // SPDX-License-Identifier: GPL-3.0-only
 
 import "../RocketBase.sol";
+import "../../interface/deposit/RocketDepositPoolInterface.sol";
+import "../../interface/minipool/RocketMinipoolQueueInterface.sol";
 import "../../interface/network/RocketNetworkFeesInterface.sol";
 import "../../lib/SafeMath.sol";
 
@@ -19,9 +21,14 @@ contract RocketNetworkFees is RocketBase, RocketNetworkFeesInterface {
     }
 
     // Get the current RP network node demand in ETH
-    // Node demand is equal to deposit pool size minus available minipool capacity
-    function getNodeDemand() override public view returns (uint256) {
-        // TODO: implement
+    // Node demand is equal to deposit pool balance minus available minipool capacity
+    function getNodeDemand() override public view returns (int256) {
+        // Load contracts
+        RocketDepositPoolInterface rocketDepositPool = RocketDepositPoolInterface(getContractAddress("rocketDepositPool"));
+        RocketMinipoolQueueInterface rocketMinipoolQueue = RocketMinipoolQueueInterface(getContractAddress("rocketMinipoolQueue"));
+        // Calculate & return
+        // TODO: check if minipool queue capacity should include empty minipools
+        return int256(rocketDepositPool.getBalance()) - int256(rocketMinipoolQueue.getTotalCapacity());
     }
 
     // Get the current RP network node fee as a fraction of 1 ETH
