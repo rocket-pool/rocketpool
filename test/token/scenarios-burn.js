8ff702212306f2bca76b05ac9631c00cc62b0b88
Jake Pospischil <moles1@gmail.com>
2020-06-23 16:51:48 +1000

    updating token burn scenarios

--- test/token/scenarios-burn.js
+++ test/token/scenarios-burn.js
@@ -1,14 +1,55 @@
-import { RocketETHToken, RocketNodeETHToken } from '../_utils/artifacts';
+import { RocketETHToken, RocketNetworkBalances, RocketNodeETHToken } from '../_utils/artifacts';
 
 
 // Burn rETH for ETH
 export async function burnReth(amount, txOptions) {
 
     // Load contracts
-    const rocketETHToken = await RocketETHToken.deployed();
+    const [
+        rocketETHToken,
+        rocketNetworkBalances,
+    ] = await Promise.all([
+        RocketETHToken.deployed(),
+        RocketNetworkBalances.deployed(),
+    ]);
+
+    // Get parameters
+    let rethExchangeRate = await rocketETHToken.getExchangeRate.call();
+
+    // Get balances
+    function getBalances() {
+        return Promise.all([
+            rocketETHToken.totalSupply.call(),
+            web3.eth.getBalance(rocketETHToken.address),
+            rocketNetworkBalances.getTotalETHBalance.call(),
+            rocketETHToken.balanceOf.call(txOptions.from),
+            web3.eth.getBalance(txOptions.from),
+        ]).then(
+            ([tokenSupply, tokenEthBalance, networkEthBalance, userTokenBalance, userEthBalance]) =>
+            ({tokenSupply, tokenEthBalance: web3.utils.toBN(tokenEthBalance), networkEthBalance, userTokenBalance, userEthBalance: web3.utils.toBN(userEthBalance)})
+        );
+    }
+
+    // Get initial balances
+    let balances1 = await getBalances();
 
     // Burn tokens
     await rocketETHToken.burn(amount, txOptions);
+
+    // Get updated balances
+    let balances2 = await getBalances();
+
+    // Calculate values
+    let burnAmount = web3.utils.toBN(amount);
+    let calcBase = web3.utils.toBN(web3.utils.toWei('1', 'ether'));
+    let expectedEthTransferred = burnAmount.mul(rethExchangeRate).div(calcBase);
+
+    // Check balances
+    assert(balances2.tokenSupply.eq(balances1.tokenSupply.sub(burnAmount)), 'Incorrect updated token supply');
+    assert(balances2.tokenEthBalance.eq(balances1.tokenEthBalance.sub(expectedEthTransferred)), 'Incorrect updated token ETH balance');
+    assert(balances2.networkEthBalance.eq(balances1.networkEthBalance.sub(expectedEthTransferred)), 'Incorrect updated token ETH balance');
+    assert(balances2.userTokenBalance.eq(balances1.userTokenBalance.sub(burnAmount)), 'Incorrect updated user token balance');
+    assert(balances2.userEthBalance.gt(balances1.userEthBalance), 'Incorrect updated user ETH balance');
 
 }
 
@@ -21,7 +62,7 @@ export async function burnNeth(amount, txOptions) {
 
     // Get balances
     function getBalances() {
-    	return Promise.all([
+        return Promise.all([
             rocketNodeETHToken.totalSupply.call(),
             web3.eth.getBalance(rocketNodeETHToken.address),
             rocketNodeETHToken.balanceOf.call(txOptions.from),
@@ -41,7 +82,7 @@ export async function burnNeth(amount, txOptions) {
     // Get updated balances
     let balances2 = await getBalances();
 
-    // Get parameters
+    // Calculate values
     let burnAmount = web3.utils.toBN(amount);
 
     // Check balances
