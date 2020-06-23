b08b1de07d9585e9453529ac3fa6395665f592cb
Jake Pospischil <moles1@gmail.com>
2020-06-23 10:59:30 +1000

    implementing network node fee formula

--- contracts/contract/network/RocketNetworkFees.sol
+++ contracts/contract/network/RocketNetworkFees.sol
@@ -6,6 +6,7 @@ import "../RocketBase.sol";
 import "../../interface/deposit/RocketDepositPoolInterface.sol";
 import "../../interface/minipool/RocketMinipoolQueueInterface.sol";
 import "../../interface/network/RocketNetworkFeesInterface.sol";
+import "../../interface/settings/RocketNetworkSettingsInterface.sol";
 import "../../lib/SafeMath.sol";
 
 // Network node demand and commission rate
@@ -32,8 +33,29 @@ contract RocketNetworkFees is RocketBase, RocketNetworkFeesInterface {
 
     // Get the current RP network node fee as a fraction of 1 ETH
     function getNodeFee() override public view returns (uint256) {
-        // TODO: implement
-        return 0.1 ether;
+        return getNodeFee(getNodeDemand());
+    }
+
+    // Get the RP network node fee for a node demand value
+    function getNodeFee(int256 _nodeDemand) override public view returns (uint256) {
+        // Calculation base values
+        uint256 calcBase = 1 ether;
+        int256 demandDivisor = 1000000000000;
+        // Get settings
+        RocketNetworkSettingsInterface rocketNetworkSettings = RocketNetworkSettingsInterface(getContractAddress("rocketNetworkSettings"));
+        uint256 minFee = rocketNetworkSettings.getMinimumNodeFee();
+        uint256 targetFee = rocketNetworkSettings.getTargetNodeFee();
+        uint256 maxFee = rocketNetworkSettings.getMaximumNodeFee();
+        uint256 demandRange = rocketNetworkSettings.getNodeFeeDemandRange();
+        // Check range bounds
+        if (_nodeDemand == 0) { return targetFee; }
+        if (_nodeDemand <= int256(demandRange) * -1) { return minFee; }
+        if (_nodeDemand >= int256(demandRange)) { return maxFee; }
+        // Get fee interpolation factor
+        uint256 t = uint256(((_nodeDemand / (demandDivisor * int256(demandRange))) ** 3) + int256(calcBase));
+        // Interpolate between min / target / max fee
+        if (t < calcBase) { return minFee.add(targetFee.sub(minFee).mul(t).div(calcBase)); }
+        else { return targetFee.add(maxFee.sub(targetFee).mul(t.sub(calcBase)).div(calcBase)); }
     }
 
 }
