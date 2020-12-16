6006c44761365a831bab744ec15b0ffe13e578de
Jake Pospischil <moles1@gmail.com>
2020-12-16 13:32:24 +1000

    implementing RPL liquidation auctions

--- contracts/contract/auction/RocketAuctionManager.sol
+++ contracts/contract/auction/RocketAuctionManager.sol
@@ -31,17 +31,23 @@ contract RocketAuctionManager is RocketBase, RocketAuctionManagerInterface {
         return rocketVault.balanceOfToken("rocketAuctionManager", getContractAddress("rocketTokenRPL"));
     }
 
-    // Get the remaining RPL balance of the contract
-    function getRemainingRPLBalance() override public view returns (uint256) {
-        return getTotalRPLBalance().sub(getAllottedRPLBalance());
-    }
-
     // Get/set the allotted RPL balance of the contract
     function getAllottedRPLBalance() override public view returns (uint256) {
         return getUintS("auction.rpl.allotted");
     }
     function setAllottedRPLBalance(uint256 _amount) private {
         setUintS("auction.rpl.allotted", _amount);
+    }
+    function increaseAllottedRPLBalance(uint256 _amount) private {
+        setAllottedRPLBalance(getAllottedRPLBalance().add(_amount));
+    }
+    function decreaseAllottedRPLBalance(uint256 _amount) private {
+        setAllottedRPLBalance(getAllottedRPLBalance().sub(_amount));
+    }
+
+    // Get the remaining (unallotted) RPL balance of the contract
+    function getRemainingRPLBalance() override public view returns (uint256) {
+        return getTotalRPLBalance().sub(getAllottedRPLBalance());
     }
 
     // Get/set the number of lots for auction
@@ -53,24 +59,20 @@ contract RocketAuctionManager is RocketBase, RocketAuctionManagerInterface {
     }
 
     // Get lot details
-    // TODO: check if active is necessary
-    function getLotActive(uint256 _index) override public view returns (bool) {
-        return getBool(keccak256(abi.encodePacked("auction.lot.active", _index)));
-    }
     function getLotStartBlock(uint256 _index) override public view returns (uint256) {
-        return getUint(keccak256(abi.encodePacked("auction.lot.start.block", _index)));
+        return getUint(keccak256(abi.encodePacked("auction.lot.block.start", _index)));
     }
     function getLotEndBlock(uint256 _index) override public view returns (uint256) {
-        return getUint(keccak256(abi.encodePacked("auction.lot.end.block", _index)));
-    }
-    function getLotTotalRPLAmount(uint256 _index) override public view returns (uint256) {
-        return getUint(keccak256(abi.encodePacked("auction.lot.rpl.total", _index)));
+        return getUint(keccak256(abi.encodePacked("auction.lot.block.end", _index)));
     }
     function getLotStartPrice(uint256 _index) override public view returns (uint256) {
         return getUint(keccak256(abi.encodePacked("auction.lot.price.start", _index)));
     }
     function getLotReservePrice(uint256 _index) override public view returns (uint256) {
         return getUint(keccak256(abi.encodePacked("auction.lot.price.reserve", _index)));
+    }
+    function getLotTotalRPLAmount(uint256 _index) override public view returns (uint256) {
+        return getUint(keccak256(abi.encodePacked("auction.lot.rpl.total", _index)));
     }
 
     // Get/set the total ETH amount bid on a lot
@@ -80,6 +82,9 @@ contract RocketAuctionManager is RocketBase, RocketAuctionManagerInterface {
     function setLotTotalBidAmount(uint256 _index, uint256 _amount) private {
         setUint(keccak256(abi.encodePacked("auction.lot.bid.total", _index)), _amount);
     }
+    function increaseLotTotalBidAmount(uint256 _index, uint256 _amount) private {
+        setLotTotalBidAmount(_index, getLotTotalBidAmount(_index).add(_amount));
+    }
 
     // Get/set the ETH amount bid on a lot by an address
     function getLotAddressBidAmount(uint256 _index, address _bidder) override public view returns (uint256) {
@@ -88,33 +93,56 @@ contract RocketAuctionManager is RocketBase, RocketAuctionManagerInterface {
     function setLotAddressBidAmount(uint256 _index, address _bidder, uint256 _amount) private {
         setUint(keccak256(abi.encodePacked("auction.lot.bid.address", _index, _bidder)), _amount);
     }
+    function increaseLotAddressBidAmount(uint256 _index, address _bidder, uint256 _amount) private {
+        setLotAddressBidAmount(_index, _bidder, getLotAddressBidAmount(_index, _bidder).add(_amount));
+    }
 
-    // Get the current RPL price for a lot
-    function getLotCurrentPrice(uint256 _index) override public view returns (uint256) {
+    // Get the RPL price for a lot at a block
+    function getLotPriceAtBlock(uint256 _index, uint256 _block) override public view returns (uint256) {
         // Get lot parameters
         uint256 startBlock = getLotStartBlock(_index);
         uint256 endBlock = getLotEndBlock(_index);
         uint256 startPrice = getLotStartPrice(_index);
         uint256 endPrice = getLotReservePrice(_index);
         // Calculate & return
-        if (block.number <= startBlock) { return startPrice; }
-        if (block.number >= endBlock) { return endPrice; }
-        uint256 tn = block.number.sub(startBlock);
+        if (_block <= startBlock) { return startPrice; }
+        if (_block >= endBlock) { return endPrice; }
+        uint256 tn = _block.sub(startBlock);
         uint256 td = endBlock.sub(startBlock);
         return startPrice.sub(startPrice.sub(endPrice).mul(tn).div(td).mul(tn).div(td));
     }
 
-    // Get the claimed RPL amount in a lot
-    function getLotClaimedRPLAmount(uint256 _index) override public view returns (uint256) {
-        uint256 claimedRplAmount = getLotTotalBidAmount(_index).div(getLotCurrentPrice(_index));
-        uint256 totalRplAmount = getLotTotalRPLAmount(_index);
-        if (claimedRplAmount > totalRplAmount) { return totalRplAmount; }
-        else { return claimedRplAmount; }
+    // Get the RPL price for a lot based on total ETH amount bid
+    function getLotPriceByTotalBids(uint256 _index) override public view returns (uint256) {
+        uint256 calcBase = 1 ether;
+        return calcBase.mul(getLotTotalBidAmount(_index)).div(getLotTotalRPLAmount(_index));
     }
 
-    // Get the remaining RPL amount in a lot
+    // Get the current RPL price for a lot
+    // Returns the clearing price if cleared, or the price at the current block otherwise
+    function getLotCurrentPrice(uint256 _index) override public view returns (uint256) {
+        uint256 blockPrice = getLotPriceAtBlock(_index, block.number);
+        uint256 bidPrice = getLotPriceByTotalBids(_index);
+        if (bidPrice > blockPrice) { return bidPrice; }
+        else { return blockPrice; }
+    }
+
+    // Get the amount of claimed RPL in a lot
+    function getLotClaimedRPLAmount(uint256 _index) override public view returns (uint256) {
+        uint256 calcBase = 1 ether;
+        return calcBase.mul(getLotTotalBidAmount(_index)).div(getLotCurrentPrice(_index));
+    }
+
+    // Get the amount of remaining RPL in a lot
     function getLotRemainingRPLAmount(uint256 _index) override public view returns (uint256) {
         return getLotTotalRPLAmount(_index).sub(getLotClaimedRPLAmount(_index));
+    }
+
+    // Check whether a lot has cleared
+    function getLotIsCleared(uint256 _index) override public view returns (bool) {
+        if (block.number >= getLotEndBlock(_index)) { return true; }
+        if (getLotPriceByTotalBids(_index) >= getLotPriceAtBlock(_index, block.number)) { return true; }
+        return false;
     }
 
     // Create a new lot for auction
@@ -122,69 +150,77 @@ contract RocketAuctionManager is RocketBase, RocketAuctionManagerInterface {
         // Load contracts
         RocketAuctionSettingsInterface rocketAuctionSettings = RocketAuctionSettingsInterface(getContractAddress("rocketAuctionSettings"));
         RocketNetworkPricesInterface rocketNetworkPrices = RocketNetworkPricesInterface(getContractAddress("rocketNetworkPrices"));
+        // Calculation base value
+        uint256 calcBase = 1 ether;
         // Get remaining RPL balance & RPL price
         uint256 remainingRplBalance = getRemainingRPLBalance();
         uint256 rplPrice = rocketNetworkPrices.getRPLPrice();
         // Check lot can be created
         require(rocketAuctionSettings.getCreateLotEnabled(), "Creating lots is currently disabled");
-        require(remainingRplBalance >= rocketAuctionSettings.getLotMinimumEthValue().div(rplPrice), "Insufficient RPL balance to create new lot");
+        require(remainingRplBalance >= calcBase.mul(rocketAuctionSettings.getLotMinimumEthValue()).div(rplPrice), "Insufficient RPL balance to create new lot");
         // Calculate lot RPL amount
         uint256 lotRplAmount = remainingRplBalance;
-        uint256 maximumLotRPlAmount = rocketAuctionSettings.getLotMaximumEthValue().div(rplPrice);
+        uint256 maximumLotRPlAmount = calcBase.mul(rocketAuctionSettings.getLotMaximumEthValue()).div(rplPrice);
         if (lotRplAmount > maximumLotRPlAmount) { lotRplAmount = maximumLotRPlAmount; }
         // Create lot
-        uint256 calcBase = 1 ether;
         uint256 lotIndex = getLotCount();
-        setBool(keccak256(abi.encodePacked("auction.lot.active", lotIndex)), true);
-        setUint(keccak256(abi.encodePacked("auction.lot.start.block", lotIndex)), block.number);
-        setUint(keccak256(abi.encodePacked("auction.lot.end.block", lotIndex)), block.number.add(rocketAuctionSettings.getLotDuration()));
-        setUint(keccak256(abi.encodePacked("auction.lot.rpl.total", lotIndex)), lotRplAmount);
+        setUint(keccak256(abi.encodePacked("auction.lot.block.start", lotIndex)), block.number);
+        setUint(keccak256(abi.encodePacked("auction.lot.block.end", lotIndex)), block.number.add(rocketAuctionSettings.getLotDuration()));
         setUint(keccak256(abi.encodePacked("auction.lot.price.start", lotIndex)), rplPrice.mul(rocketAuctionSettings.getStartingPriceRatio()).div(calcBase));
         setUint(keccak256(abi.encodePacked("auction.lot.price.reserve", lotIndex)), rplPrice.mul(rocketAuctionSettings.getReservePriceRatio()).div(calcBase));
+        setUint(keccak256(abi.encodePacked("auction.lot.rpl.total", lotIndex)), lotRplAmount);
         // Increment lot count & increase allotted RPL balance
         setLotCount(lotIndex.add(1));
         increaseAllottedRPLBalance(lotRplAmount);
     }
 
     // Bid on a lot
-    function bidOnLot(uint256 _index) override external payable onlyLatestContract("rocketAuctionManager", address(this)) {
+    function bid(uint256 _lotIndex) override external payable onlyLatestContract("rocketAuctionManager", address(this)) {
         // Load contracts
         RocketAuctionSettingsInterface rocketAuctionSettings = RocketAuctionSettingsInterface(getContractAddress("rocketAuctionSettings"));
         RocketDepositPoolInterface rocketDepositPool = RocketDepositPoolInterface(getContractAddress("rocketDepositPool"));
         // Check lot can be bid on
         require(rocketAuctionSettings.getBidOnLotEnabled(), "Bidding on lots is currently disabled");
-        require(getLotActive(_index), "Lot is no longer active");
-        require(block.number < getLotEndBlock(_index), "Lot bidding period has concluded");
+        require(block.number < getLotEndBlock(_lotIndex), "Lot bidding period has concluded");
         // Check lot has RPL remaining
-        uint256 remainingRplAmount = getLotRemainingRPLAmount(_index);
+        uint256 remainingRplAmount = getLotRemainingRPLAmount(_lotIndex);
         require(remainingRplAmount > 0, "Lot RPL allocation has been exhausted");
         // Calculate the bid amount
-        uint256 maxBidAmount = remainingRplAmount.mul(getLotCurrentPrice(_index));
         uint256 bidAmount = msg.value;
-        if (bidAmount > maxBidAmount) { bidAmount = maxBidAmount; }
+        uint256 calcBase = 1 ether;
+        uint256 maximumBidAmount = remainingRplAmount.mul(getLotPriceAtBlock(_lotIndex, block.number).div(calcBase));
+        if (bidAmount > maximumBidAmount) { bidAmount = maximumBidAmount; }
         // Increase lot bid amounts
-        increaseLotTotalBidAmount(_index, bidAmount);
-        increaseLotAddressBidAmount(_index, msg.sender, bidAmount);
+        increaseLotTotalBidAmount(_lotIndex, bidAmount);
+        increaseLotAddressBidAmount(_lotIndex, msg.sender, bidAmount);
         // Transfer bid amount to deposit pool
         rocketDepositPool.recycleLiquidatedStake{value: bidAmount}();
         // Refund excess ETH to sender
-        if (bidAmount < msg.value) { msg.sender.transfer(msg.value.sub(bidAmount)); }
+        if (msg.value > bidAmount) { msg.sender.transfer(msg.value.sub(bidAmount)); }
     }
 
-    // Increase/decrease the allotted RPL balance
-    function increaseAllottedRPLBalance(uint256 _amount) private {
-        setAllottedRPLBalance(getAllottedRPLBalance().add(_amount));
-    }
-    function decreaseAllottedRPLBalance(uint256 _amount) private {
-        setAllottedRPLBalance(getAllottedRPLBalance().sub(_amount));
-    }
-
-    // Increase ETH bid amounts on a lot
-    function increaseLotTotalBidAmount(uint256 _index, uint256 _amount) private {
-        setLotTotalBidAmount(_index, getLotTotalBidAmount(_index).add(_amount));
-    }
-    function increaseLotAddressBidAmount(uint256 _index, address _bidder, uint256 _amount) private {
-        setLotAddressBidAmount(_index, _bidder, getLotAddressBidAmount(_index, _bidder).add(_amount));
+    // Claim RPL from a lot
+    function claim(uint256 _lotIndex) override external payable onlyLatestContract("rocketAuctionManager", address(this)) {
+        // Get lot price info
+        uint256 blockPrice = getLotPriceAtBlock(_lotIndex, block.number);
+        uint256 bidPrice = getLotPriceByTotalBids(_lotIndex);
+        // Check lot can be claimed from
+        require(block.number >= getLotEndBlock(_lotIndex) || bidPrice >= blockPrice, "Lot has not cleared yet");
+        // Get & check address bid amount
+        uint256 bidAmount = getLotAddressBidAmount(_lotIndex, msg.sender);
+        require(bidAmount > 0, "Address has no RPL to claim");
+        // Calculate current lot price
+        uint256 currentPrice;
+        if (bidPrice > blockPrice) { currentPrice = bidPrice; }
+        else { currentPrice = blockPrice; }
+        // Calculate RPL claim amount
+        uint256 calcBase = 1 ether;
+        uint256 rplAmount = calcBase.mul(bidAmount).div(currentPrice);
+        // Transfer RPL to bidder
+        RocketVaultInterface rocketVault = RocketVaultInterface(getContractAddress("rocketVault"));
+        rocketVault.withdrawToken(msg.sender, getContractAddress("rocketTokenRPL"), rplAmount);
+        // Update address bid amount
+        setLotAddressBidAmount(_lotIndex, msg.sender, 0);
     }
 
 }
