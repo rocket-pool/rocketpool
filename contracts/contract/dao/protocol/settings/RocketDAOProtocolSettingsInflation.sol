28ff8e2310118a99e9edcd43524bc1848350a8ec
Kane Wallmann <kane@rocketpool.net>
2021-06-01 14:42:22 +1000

    Add call to inflate RPL before an update to inflation rate

--- contracts/contract/dao/protocol/settings/RocketDAOProtocolSettingsInflation.sol
+++ contracts/contract/dao/protocol/settings/RocketDAOProtocolSettingsInflation.sol
@@ -4,6 +4,7 @@ pragma solidity 0.7.6;
 
 import "./RocketDAOProtocolSettings.sol";
 import "../../../../interface/dao/protocol/settings/RocketDAOProtocolSettingsInflationInterface.sol";
+import "../../../../interface/token/RocketTokenRPLInterface.sol";
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
@@ -32,18 +33,27 @@ contract RocketDAOProtocolSettingsInflation is RocketDAOProtocolSettings, Rocket
     function setSettingUint(string memory _settingPath, uint256 _value) override public onlyDAOProtocolProposal {
         // Some safety guards for certain settings
         // The start time for inflation must be in the future and cannot be set again once started
-        if(keccak256(bytes(_settingPath)) == keccak256(bytes("rpl.inflation.interval.start"))) {
-                // Must be a future timestamp
-                require(_value > block.timestamp, "Inflation interval start time must be in the future");
-                // If it's already set and started, a new start block cannot be set
-                if(getInflationIntervalStartTime() > 0) {
-                    require(getInflationIntervalStartTime() > block.timestamp, "Inflation has already started");
-                }
+        bytes32 settingKey = keccak256(bytes(_settingPath));
+        if(settingKey == keccak256(bytes("rpl.inflation.interval.start"))) {
+            // Must be a future timestamp
+            require(_value > block.timestamp, "Inflation interval start time must be in the future");
+            // If it's already set and started, a new start block cannot be set
+            if(getInflationIntervalStartTime() > 0) {
+                require(getInflationIntervalStartTime() > block.timestamp, "Inflation has already started");
+            }
+        } else if(settingKey == keccak256(bytes("rpl.inflation.interval.rate"))) {
+            // RPL contract address
+            address rplContractAddress = getContractAddressUnsafe("rocketTokenRPL");
+            if(rplContractAddress != address(0x0)) {
+                // Force inflation at old rate before updating inflation rate
+                RocketTokenRPLInterface rplContract = RocketTokenRPLInterface(rplContractAddress);
+                // Mint any new tokens from the RPL inflation
+                rplContract.inflationMintTokens();
+            }
         }
         // Update setting now
         setUint(keccak256(abi.encodePacked(settingNameSpace, _settingPath)), _value);
-    } 
-    
+    }
 
     /*** RPL Contract Settings *****************************************/
 
