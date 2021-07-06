7ad9ab7fee3336a6ab1cd9dee2ee637124aab72e
Kane Wallmann <kane@rocketpool.net>
2021-07-06 10:21:10 +1000

    Fix code style inconsistency

--- contracts/contract/token/RocketTokenRPL.sol
+++ contracts/contract/token/RocketTokenRPL.sol
@@ -131,11 +131,11 @@ contract RocketTokenRPL is RocketBase, ERC20Burnable, RocketTokenRPLInterface {
         return _inflationCalculate(intervalsSinceLastMint);
     }
 
-    function _inflationCalculate(uint256 intervalsSinceLastMint) private view returns (uint256) {
+    function _inflationCalculate(uint256 _intervalsSinceLastMint) private view returns (uint256) {
         // The inflation amount
         uint256 inflationTokenAmount = 0;
         // Only update  if last interval has passed and inflation rate is > 0
-        if(intervalsSinceLastMint > 0) {
+        if(_intervalsSinceLastMint > 0) {
             // Optimisation
             uint256 inflationRate = getInflationIntervalRate();
             if(inflationRate > 0) {
@@ -143,7 +143,7 @@ contract RocketTokenRPL is RocketBase, ERC20Burnable, RocketTokenRPLInterface {
                 uint256 totalSupplyCurrent = totalSupply();
                 uint256 newTotalSupply = totalSupplyCurrent;
                 // Compute inflation for total inflation intervals elapsed
-                for (uint256 i = 0; i < intervalsSinceLastMint; i++) {
+                for (uint256 i = 0; i < _intervalsSinceLastMint; i++) {
                     newTotalSupply = newTotalSupply.mul(inflationRate).div(10**18);
                 }
                 // Return inflation amount
