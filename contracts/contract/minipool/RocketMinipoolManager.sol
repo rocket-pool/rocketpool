1a54118ae0f4b2e2a2423ce9a65877867fa66f29
Joe Clapis <jclapis@outlook.com>
2021-07-27 01:56:49 -0400

    Added getMinipoolCountPerStatus to RocketMinipoolManager

--- contracts/contract/minipool/RocketMinipoolManager.sol
+++ contracts/contract/minipool/RocketMinipoolManager.sol
@@ -45,6 +45,53 @@ contract RocketMinipoolManager is RocketBase, RocketMinipoolManagerInterface {
         return getUint(keccak256(bytes("minipools.staking.count")));
     }
 
+    // Get the number of minipools in each status.
+    // Returns the counts for Initialized, Prelaunch, Staking, Withdrawable, and Dissolved in that order.
+    function getMinipoolCountPerStatus(uint256 offset, uint256 limit) override external view 
+    returns (uint256, uint256, uint256, uint256, uint256) {
+        uint256 initializedCount = 0;
+        uint256 prelaunchCount = 0;
+        uint256 stakingCount = 0;
+        uint256 withdrawableCount = 0;
+        uint256 dissolvedCount = 0;
+
+        // Iterate over the requested minipool range
+        uint256 totalMinipools = getMinipoolCount();
+        uint256 max = offset.add(limit);
+        if (max > totalMinipools || limit == 0) { max = totalMinipools; }
+        for (uint256 i = offset; i < max; i++) {
+            // Get the minipool at index i
+            address minipoolAddress = getMinipoolAt(i);
+            RocketMinipool minipool = RocketMinipoolInterface(minipoolAddress);
+
+            // Get the minipool's status, and update the appropriate counter
+            MinipoolStatus status = minipool.getStatus();
+            if (status == MinipoolStatus.Initialized) {
+                initializedCount++;
+            }
+            else if (status == MinipoolStatus.Prelaunch) {
+                prelaunchCount++;
+            }
+            else if (status == MinipoolStatus.Staking) {
+                stakingCount++;
+            }
+            else if (status == MinipoolStatus.Withdrawable) {
+                withdrawableCount++;
+            }
+            else if (status == MinipoolStatus.Dissolved) {
+                dissolvedCount++;
+            }
+        }
+
+        // Return all of the counters
+        return (
+            initializedCount, 
+            prelaunchCount, 
+            stakingCount, 
+            withdrawableCount, 
+            dissolvedCount);
+    }
+
     // Get a network minipool address by index
     function getMinipoolAt(uint256 _index) override external view returns (address) {
         AddressSetStorageInterface addressSetStorage = AddressSetStorageInterface(getContractAddress("addressSetStorage"));
