0cc6b065c6cf71a57f7e05193f13c5a587452000
Kane Wallmann <kane@rocketpool.net>
2022-06-02 11:32:26 +1000

    Propogate revert messages to caller when delegate call reverts

--- contracts/contract/node/RocketNodeDistributor.sol
+++ contracts/contract/node/RocketNodeDistributor.sol
@@ -26,7 +26,7 @@ contract RocketNodeDistributor is RocketNodeDistributorStorageLayout {
             calldatacopy(0x0, 0x0, calldatasize())
             let result := delegatecall(gas(), _target, 0x0, calldatasize(), 0x0, 0)
             returndatacopy(0x0, 0x0, returndatasize())
-            switch result case 0 {revert(0, 0)} default {return (0, returndatasize())}
+            switch result case 0 {revert(0, returndatasize())} default {return (0, returndatasize())}
         }
     }
 }
