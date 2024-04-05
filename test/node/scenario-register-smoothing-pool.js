f3ec085fa7cbad4e111e560aa836463e3cefa356
Kane Wallmann <kane@rocketpool.net>
2024-04-05 10:37:48 +1000

    Fix failing tests

--- test/node/scenario-register-smoothing-pool.js
+++ test/node/scenario-register-smoothing-pool.js
@@ -1,10 +1,11 @@
-import { RocketNodeManager } from '../_utils/artifacts';
+import { RocketNodeManager, RocketNodeManagerNew } from '../_utils/artifacts';
+import { upgradeExecuted } from '../_utils/upgrade';
 
 
 // Register a node
 export async function setSmoothingPoolRegistrationState(state, txOptions) {
     // Load contracts
-    const rocketNodeManager = await RocketNodeManager.deployed();
+    const rocketNodeManager = await upgradeExecuted() ? await RocketNodeManagerNew.deployed() : await RocketNodeManager.deployed();
 
     // Register
     await rocketNodeManager.setSmoothingPoolRegistrationState(state, txOptions);
